1effa49cbf63b429ce1f3664ce0140f80d43624a
smartie2076 <gitMA18rli>
2019-10-23 14:46:46 +0200

    Change import to relative paths

--- mvs_eland_tool.py
+++ mvs_eland_tool.py
@@ -31,12 +31,12 @@ import logging
 
 # Loading all child functions
 try:
-    from code.A_initialization import initializing
-    from code.B0_data_input import data_input
-    from code.C0_data_processing import data_processing
-    from code.D0_modelling_and_optimization import modelling
-    from code.E0_evaluation import evaluation
-    from code.F0_output import output_processing
+    from .code.A_initialization import initializing
+    from .code.B0_data_input import data_input
+    from .code.C0_data_processing import data_processing
+    from .code.D0_modelling_and_optimization import modelling
+    from .code.E0_evaluation import evaluation
+    from .code.F0_output import output_processing
 
 except ModuleNotFoundError:
     from code.A_initialization import initializing
