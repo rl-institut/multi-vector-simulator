f47c55b1b71d276f8d05c1e2df373341813f1bfd
smartie2076 <gitMA18rli>
2019-10-25 16:06:18 +0200

    Update change date

--- mvs_eland_tool.py
+++ mvs_eland_tool.py
@@ -49,7 +49,7 @@ except ModuleNotFoundError:
 def main():
     # Display welcome text
     version = '0.0.1' #update_me Versioning scheme: Major release.Minor release.Patches
-    date = '19.09.2019' #update_me Update date
+    date = '25.10.2019' #update_me Update date
 
     welcome_text = \
         '\n \n Multi-Vector Simulation Tool (MVS) V' + version + ' ' + \
