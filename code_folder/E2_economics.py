9384e96c4f814c2abc170ac2da3bcf96c42087b6
smartie2076 <gitMA18rli>
2019-11-01 14:48:33 +0100

    Update: Call "value" of parameters

--- code_folder/E2_economics.py
+++ code_folder/E2_economics.py
@@ -8,47 +8,47 @@ class economics:
             costs_total = 0
             cost_om = 0
             # Calculation of connected parameters:
-            if helpers.all_list_in_dict(dict_asset, ['lifetime_capex_var', 'capex_fix', 'optimal_additional_capacity']) == True \
-                    and dict_asset['optimal_additional_capacity'] > 0:
+            if helpers.all_list_in_dict(dict_asset, ['lifetime_capex_var', 'capex_fix', 'optimizedAddCap']) == True \
+                    and dict_asset['optimizedAddCap']['value'] > 0:
                 # total investments including fix prices
-                costs_investment = dict_asset['optimal_additional_capacity'] * dict_asset['lifetime_capex_var'] + dict_asset['capex_fix']
+                costs_investment = dict_asset['optimizedAddCap']['value'] * dict_asset['lifetime_capex_var']['value'] + dict_asset['capex_fix']['value']
                 costs_total = helpers.add_costs_and_total(dict_asset, 'costs_investment', costs_investment, costs_total)
 
-            if helpers.all_list_in_dict(dict_asset, ['capex_var', 'capex_fix', 'optimal_additional_capacity']) == True \
-                    and dict_asset['optimal_additional_capacity'] > 0:
+            if helpers.all_list_in_dict(dict_asset, ['capex_var', 'capex_fix', 'optimizedAddCap']) == True \
+                    and dict_asset['optimizedAddCap']['value'] > 0:
                 # investments including fix prices, only upfront costs at t=0
-                costs_upfront = dict_asset['optimal_additional_capacity'] + dict_asset['capex_var'] + dict_asset['capex_fix']
+                costs_upfront = dict_asset['optimizedAddCap']['value'] + dict_asset['capex_var']['value'] + dict_asset['capex_fix']['value']
                 costs_total = helpers.add_costs_and_total(dict_asset, 'costs_upfront', costs_upfront, costs_total)
 
             if helpers.all_list_in_dict(dict_asset, ['annual_total_flow', 'lifetime_opex_var']) == True:
-                costs_opex_var = dict_asset['lifetime_opex_var'] * dict_asset['annual_total_flow']
+                costs_opex_var = dict_asset['lifetime_opex_var']['value'] * dict_asset['annual_total_flow']['value']
                 costs_total = helpers.add_costs_and_total(dict_asset, 'costs_opex_var', costs_opex_var, costs_total)
                 cost_om = helpers.add_costs_and_total(dict_asset, 'costs_opex_var', costs_opex_var, cost_om)
 
             if helpers.all_list_in_dict(dict_asset, ['price', 'annual_total_flow']) == True:
-                costs_energy = dict_asset['price'] * dict_asset['annual_total_flow']
+                costs_energy = dict_asset['price']['value'] * dict_asset['annual_total_flow']['value']
                 cost_om = helpers.add_costs_and_total(dict_asset, 'costs_energy', costs_energy, cost_om)
 
-            if helpers.all_list_in_dict(dict_asset, ['annual_total_flow', 'lifetime_opex_var', 'cap_installed', 'optimal_additional_capacity']) == True:
-                cap = dict_asset['cap_installed']
-                if 'optimal_additional_capacity' in dict_asset:
-                    cap += dict_asset['optimal_additional_capacity']
+            if helpers.all_list_in_dict(dict_asset, ['annual_total_flow', 'lifetime_opex_var', 'installedCap', 'optimizedAddCap']) == True:
+                cap = dict_asset['installedCap']['value']
+                if 'optimizedAddCap' in dict_asset:
+                    cap += dict_asset['optimizedAddCap']['value']
 
-                costs_opex_fix = dict_asset['lifetime_opex_fix']*cap
+                costs_opex_fix = dict_asset['lifetime_opex_fix']['value']*cap
                 costs_total = helpers.add_costs_and_total(dict_asset, 'costs_opex_fix', costs_opex_fix, costs_total)
                 cost_om = helpers.add_costs_and_total(dict_asset, 'costs_opex_fix', costs_opex_fix, cost_om)
 
-            dict_asset.update({'costs_total': costs_total,
-                               'cost_om': cost_om})
+            dict_asset.update({'costs_total': {'value': costs_total, 'unit': 'currency'},
+                               'cost_om': {'value': cost_om, 'unit': 'currency'}})
 
-            dict_asset.update({'annuity_total': dict_asset['costs_total'] * economic_data['crf'],
-                               'annuity_om': dict_asset['cost_om'] * economic_data['crf']})
+            dict_asset.update({'annuity_total': {'value': dict_asset['costs_total']['value'] * economic_data['crf']['value'], 'unit': 'currency/year'},
+                               'annuity_om': {'value': dict_asset['cost_om']['value'] * economic_data['crf']['value'], 'unit': 'currency/year'}})
         return
 
 class helpers:
     def add_costs_and_total(dict_asset, name, value, total_costs):
         total_costs += value
-        dict_asset.update({name: value})
+        dict_asset.update({name: {'value': value}})
         return total_costs
 
     def all_list_in_dict(dict_asset, list):
