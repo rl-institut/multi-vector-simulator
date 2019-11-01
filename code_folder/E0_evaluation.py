0cfb12079d659f106abc2704deb631627b3d8e9c
smartie2076 <gitMA18rli>
2019-11-01 14:57:14 +0100

    Recoding of evaluation with new data strucutre

--- code_folder/E0_evaluation.py
+++ code_folder/E0_evaluation.py
@@ -1,39 +1,45 @@
 import logging
 import oemof.outputlib as outputlib
+import pandas as pd
+import pprint as pp
 try:
-    from .E1_process_results import process_results
-    from .E2_economics import economics
+    from .code_folder.E1_process_results import process_results
+    from .code_folder.E2_economics import economics
 except ImportError:
     from code_folder.E1_process_results import process_results
     from code_folder.E2_economics import economics
 
 class evaluation:
-    def evaluate_dict(dict_values, results_main, results_meta, dict_model):
+    def evaluate_dict(dict_values, results_main, results_meta):
         bus_data={}
-        for bus in dict_model['busses']:
+        # Store all information related to busses in bus_data
+        for bus in dict_values['energyBusses']:
             # Read all energy flows from busses
             bus_data.update({bus: outputlib.views.node(results_main, bus)})
 
-        for storage in dict_model['storages']:
-            # Read all energy flows from storage
-            bus_data.update({storage: outputlib.views.node(results_main, storage)})
+        # Evaluate timeseries and store to a large DataFrame for each bus:
+        bus_data_timeseries = process_results.get_timeseries_per_bus(dict_values, bus_data)
+
+        # Store all information related to storages in bus_data, as storage capacity acts as a bus
+        for sector in dict_values['energyStorage']:
+            for storage in dict_values['energyStorage'][sector]:
+                bus_data.update({dict_values['energyStorage'][sector][storage]['label']:
+                    outputlib.views.node(results_main, dict_values['energyStorage'][sector][storage]['label'])})
+                process_results.get_storage_results(dict_values['simulation_settings'],
+                                                    bus_data[dict_values['energyStorage'][sector][storage]['label']],
+                                                    dict_values['energyStorage'][sector][storage])
+                economics.get_costs(dict_values['energyStorage'][sector][storage], dict_values['economic_data'])
+
+        print(bus_data.keys())
+        for asset in dict_values['energyConversion']:
+            process_results.get_results(dict_values['simulation_settings'], bus_data, dict_values['energyConversion'][asset])
+            economics.get_costs(dict_values['energyConversion'][asset], dict_values['economic_data'])
+
+        for group in ['energyProduction', 'energyConsumption']:
+            for sector in dict_values[group]:
+                for asset in dict_values[group][sector]:
+                    process_results.get_results(dict_values['simulation_settings'], bus_data, dict_values[group][sector][asset])
+                    economics.get_costs(dict_values[group][sector][asset], dict_values['economic_data'])
 
         logging.info('Evaluating optimized capacities and dispatch.')
-        for item in dict_values.keys():
-            process_results.check_for_evaluation_keys(dict_values, dict_values[item], bus_data)
-            for subitem in dict_values[item].keys():
-                process_results.check_for_evaluation_keys(dict_values, dict_values[item][subitem], bus_data)
-                if isinstance(dict_values[item][subitem], dict):
-                    for subsubitem in dict_values[item][subitem].keys():
-                        process_results.check_for_evaluation_keys(dict_values, dict_values[item][subitem][subsubitem], bus_data)
-
-        logging.info('Calculating costs of asset investment and usage.')
-        for item in dict_values.keys():
-            economics.get_costs(dict_values[item], dict_values['economic_data'])
-            for subitem in dict_values[item].keys():
-                economics.get_costs(dict_values[item][subitem], dict_values['economic_data'])
-                if isinstance(dict_values[item][subitem], dict):
-                    for subsubitem in dict_values[item][subitem].keys():
-                        economics.get_costs(dict_values[item][subitem][subsubitem], dict_values['economic_data'])
-
         return
