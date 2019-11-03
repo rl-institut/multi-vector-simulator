53f2b95e789cbc425489e5a85d630577776639c4
smartie2076 <gitMA18rli>
2019-11-03 23:21:46 +0200

    Update outputs to new data structure, delete loose ends

--- code_folder/F0_output.py
+++ code_folder/F0_output.py
@@ -7,128 +7,59 @@ import json
 import numpy
 import pandas as pd
 import logging
+import matplotlib.pyplot as plt
 
 class output_processing():
     def evaluate_dict(dict_values):
         logging.info('Summarizing simulation results to results_timeseries and results_scalars_assets.')
-        scalars = {}
+        for sector in dict_values['project_data']['sectors']:
+            sector_name = dict_values['project_data']['sectors'][sector]
+            total_demand = pd.Series([0 for i in dict_values['simulation_settings']['time_index']],
+                                                   index = dict_values['simulation_settings']['time_index'])
 
-        # these dictionaries will save the necessary information to plot the additional capacities and the annuity costs of the assets
-        capacities = {}
-        annuity_costs = {}
+            for asset in dict_values['energyConsumption'][sector_name]:
+                total_demand = total_demand + dict_values['energyConsumption'][sector_name][asset]['flow']
 
-        for sector in ['electricity', 'heat']:
-            results_timeseries = {'total_demand_'+sector:
-                                      pd.Series([0 for i in dict_values['simulation_settings']['time_index']],
-                                                   index = dict_values['simulation_settings']['time_index'])}
-            results_scalars_assets = {}
-            results_scalars_other = {}
+            #todo this should actually link to C0: helpers.bus_suffix
+            dict_values['optimizedFlows'][sector_name+' bus']['Total demand ' + sector_name] = total_demand
 
-            for item in dict_values.keys():
-                helpers.write_results(dict_values[item], results_scalars_assets, results_scalars_other, results_timeseries, sector)
-                for subitem in dict_values[item].keys():
-                    helpers.write_results(dict_values[item][subitem], results_scalars_assets, results_scalars_other, results_timeseries, sector)
-                    if isinstance(dict_values[item][subitem], dict):
-                        for subsubitem in dict_values[item][subitem].keys():
-                            helpers.write_results(dict_values[item][subitem][subsubitem], results_scalars_assets, results_scalars_other, results_timeseries, sector)
+            plots.flows(dict_values['simulation_settings'], dict_values['project_data'],
+                        dict_values['optimizedFlows'][sector_name+' bus'], sector, 14)
+            plots.flows(dict_values['simulation_settings'], dict_values['project_data'],
+                        dict_values['optimizedFlows'][sector_name+' bus'], sector, 365)
 
-            # save the information regarding additional optimal capacities and annuity costs
-            for asset in results_scalars_assets:
-                if 'optimizedAddCap' in results_scalars_assets[asset]:
-                    capacities[asset] = results_scalars_assets[asset]['optimizedAddCap']
-                if 'annuity_total' in results_scalars_assets[asset]:
-                    annuity_costs[asset] = results_scalars_assets[asset]['annuity_total']
-
-            results_timeseries = pd.DataFrame.from_dict(results_timeseries)
-            results_scalars_assets = pd.DataFrame.from_dict(results_scalars_assets).transpose()
-            results_scalars_other = pd.DataFrame.from_dict(results_scalars_other).transpose()
-
-            results_timeseries_output_file = 'timeseries_'+sector+'.xlsx'
-            results_timeseries.to_excel(dict_values['simulation_settings']['path_output_folder'] + '/'+ results_timeseries_output_file)
-            logging.info('Saved resulting timeseries to: %s.', results_timeseries_output_file)
-
-            scalars.update({sector: {'results_scalars_assets': results_scalars_assets,
-                                     'results_scalars_other': results_scalars_other}})
-
-            plots.flows(dict_values['simulation_settings'], dict_values['project_data'], results_timeseries, sector, 14)
-            plots.flows(dict_values['simulation_settings'], dict_values['project_data'], results_timeseries, sector, 365)
-
+        helpers.store_timeseries_all_busses_to_excel(dict_values)
         # plot the capacities and the annuity costs
-        plots.capacities(dict_values['simulation_settings'],dict_values['project_data'],capacities)
-        plots.costs(dict_values['simulation_settings'],dict_values['project_data'],annuity_costs)
+        plots.capacities(dict_values['simulation_settings'],dict_values['project_data'], dict_values['kpi']['scalar_matrix']['optimizedAddCap'])
+        plots.costs(dict_values['simulation_settings'],dict_values['project_data'], dict_values['kpi']['cost_matrix']['label'], dict_values['kpi']['cost_matrix']['annuity_total'])
 
         # Write everything to file with multipe tabs
         results_scalar_output_file = '/scalars' + '.xlsx'
         with pd.ExcelWriter(dict_values['simulation_settings']['path_output_folder'] + results_scalar_output_file) as open_file:  # doctest: +SKIP
-            for sector in scalars.keys():
-                scalars[sector]['results_scalars_assets'].to_excel(open_file, sheet_name=sector)
+            for kpi_set in dict_values['kpi']:
+                dict_values['kpi'][kpi_set].to_excel(open_file, sheet_name=sector)
                 logging.info('Saved scalar results to: %s, tab %s.', results_scalar_output_file, sector)
-                scalars[sector]['results_scalars_other'].to_excel(open_file, sheet_name=sector+'_kpi')
-                logging.info('Saved scalar results to: %s, tab %s.', results_scalar_output_file, sector+'_kpi')
 
         return
 
 class helpers:
-    def write_results(dict_asset, results_scalars_assets, results_scalars_other, results_timeseries, sector):
-        if isinstance(dict_asset, dict):
-            logging.debug('Storing results of asset %s in scalar and timeseries results.', dict_asset['label'])
-            if ('input_bus_name' in dict_asset) or ('output_bus_name' in dict_asset):
-                results_scalars_assets.update({dict_asset['label']: {}})
-                helpers.write_scalars_assets(dict_asset, results_scalars_assets, results_timeseries, sector)
-            elif dict_asset['label'] == 'project_data':
-                results_scalars_other.update({dict_asset['label']: {}})
-                helpers.write_scalars_other(dict_asset, results_scalars_other)
-            else:
-                logging.debug('Entry %s does not have a distinct bus or belongs in evaluated group.', dict_asset['label'])
+    def store_timeseries_all_busses_to_excel(dict_values):
+        #todo this should be moved to f0_output
+        timeseries_output_file = '/timeseries_all_busses' + '.xlsx'
+        with pd.ExcelWriter(
+                dict_values['simulation_settings']['path_output_folder'] + timeseries_output_file) as open_file:  # doctest: +SKIP
+            for bus in dict_values['optimizedFlows']:
+                dict_values['optimizedFlows'][bus].to_excel(open_file, sheet_name=bus)
+                dict_values['optimizedFlows'][bus].plot()
+                plt.savefig(dict_values['simulation_settings']['path_output_folder'] + '/' + bus + ' flows.png',
+                            bbox_inches="tight")
+                #if bus == 'Electricity (LES) bus' or bus == 'Electricity (DSO) bus':
+                #    plt.show()
+                plt.close()
+                plt.clf()
+                plt.cla()
 
-        return
-
-    def write_scalars_assets(dict_asset, results_scalars_assets, results_timeseries, sector):
-        keyword_list = ['type',
-                        'timeseries_soc',
-                        'optimizedAddCap',
-                        'total_flow',
-                        'annual_total_flow',
-                        'peak_flow',
-                        'average_flow',
-                        'flow',
-                        'costs_investment',
-                        'costs_upfront',
-                        'costs_opex_var',
-                        'costs_opex_fix',
-                        'cost_om',
-                        'costs_total',
-                        'annuity_total',
-                        'annuity_om']
-
-        for key in keyword_list:
-            if key in dict_asset:
-                if key == 'flow':
-                    if ('output_bus_name' in dict_asset and dict_asset['output_bus_name'] == sector) \
-                            or ('input_bus_name' in dict_asset and dict_asset['input_bus_name'] == sector):
-                        if 'parent' in dict_asset and dict_asset['parent'] == 'demand':
-                            results_timeseries.update({'total_demand_'+sector:
-                                                           results_timeseries['total_demand_'+sector]+dict_asset[key]})
-                        elif dict_asset['label'][-3:]=='out':
-                            results_timeseries.update({dict_asset['label']: -dict_asset[key]})
-                        else:
-                            results_timeseries.update({dict_asset['label']: dict_asset[key]})
-                elif key == 'timeseries_soc':
-                    results_timeseries.update({dict_asset['label'] + '_soc': dict_asset[key]})
-                else:
-                    results_scalars_assets[dict_asset['label']].update({key: dict_asset[key]})
-        return
-
-    def write_scalars_other(dict_asset, results_scalars_other):
-        keyword_list = ['costs_investment',
-                        'cost_om',
-                        'costs_total',
-                        'annuity_total',
-                        'annuity_om'] #'costs_energy',
-
-        for key in keyword_list:
-            if key in dict_asset:
-                results_scalars_other[dict_asset['label']].update({key: dict_asset[key]})
+        logging.info('Saved flows at busses to: %s.', timeseries_output_file)
         return
 
     def store_as_json(dict_values, file_name):
