787c5516aaa3c16a07f833e6b4b6a76ea3cfa44d
smartie2076 <gitMA18rli>
2019-11-25 11:31:43 +0100

    Added pytest: Blacks code formatting

--- tests/tests.py
+++ tests/tests.py
@@ -7,8 +7,10 @@ from ..mvs_eland_tool import main
 def test_run_smoothly():
     assert main(test=True) == 1
 
-import black
-
-def test_blacks():
+def test_blacks_main():
     r = os.system("black --check mvs_eland_tool.py")
+    assert r == 0
+
+def test_blacks_code_folder():
+    r = os.system("black --check code_folder/*.py")
     assert r == 0
