bb81e8b3eeef7cf8b8311c0fa82a28332c2f7003
smartie2076 <gitMA18rli>
2019-11-25 11:41:08 +0100

    Added comments to tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,12 +5,15 @@ import os
 from ..mvs_eland_tool import main
 
 def test_run_smoothly():
+    # Ensure that code does not terminate
     assert main(test=True) == 1
 
 def test_blacks_main():
+    # Testing code formatting
     r = os.system("black --check mvs_eland_tool.py")
     assert r == 0
 
 def test_blacks_code_folder():
+    # Testing code formatting
     r = os.system("black --check code_folder/*.py")
     assert r == 0
