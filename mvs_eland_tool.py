8e139b7e912bb9ae9dfde9370e9bebd0ded1a5bb
smartie2076 <gitMA18rli>
2019-11-25 14:20:21 +0100

    Executed blacks (new version)

--- mvs_eland_tool.py
+++ mvs_eland_tool.py
@@ -49,11 +49,12 @@ except ModuleNotFoundError:
     from code_folder.E0_evaluation import evaluation
     from code_folder.F0_output import output_processing
 
+
 def main(**kwargs):
     # Display welcome text
     version = (
-        "0.0.1"
-    )  # update_me Versioning scheme: Major release.Minor release.Patches
+        "0.0.1"  # update_me Versioning scheme: Major release.Minor release.Patches
+    )
     date = "31.10.2019"  # update_me Update date
 
     welcome_text = (
