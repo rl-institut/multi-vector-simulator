4f082b211b23e077fa429d3cdf2f2b1ce5b7abf3
smartie2076 <gitMA18rli>
2019-11-25 11:08:18 +0100

    Add pytest: Blacks (code formatting)

--- tests/tests.py
+++ tests/tests.py
@@ -6,3 +6,9 @@ from ..mvs_eland_tool import main
 
 def test_run_smoothly():
     assert main(test=True) == 1
+
+import black
+
+def test_blacks():
+    r = os.system("black --check mvs_eland_tool.py")
+    assert r == 0
