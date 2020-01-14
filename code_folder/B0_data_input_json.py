7bcd5f5ac2a30e6d15900047d58c25331969237e
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-14 18:26:08 +0100

    Add docstring for data input code

--- code_folder/B0_data_input_json.py
+++ code_folder/B0_data_input_json.py
@@ -4,7 +4,17 @@ import json
 
 
 class data_input:
-    def get(user_input):
+    def get(self, user_input):
+        """
+
+        Parameters
+        ----------
+        user_input
+
+        Returns
+        -------
+
+        """
         with open(user_input["path_input_file"]) as json_file:
             dict_values = json.load(json_file)
         return dict_values
