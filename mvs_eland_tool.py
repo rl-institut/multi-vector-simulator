9eb7358d73c517fa4a449277393dbfa95333cf58
smartie2076 <gitMA18rli>
2020-01-21 10:44:36 +0100

    Formatting with blacks

--- mvs_eland_tool.py
+++ mvs_eland_tool.py
@@ -83,7 +83,7 @@ def main(**kwargs):
     logging.debug("Accessing script: A1_csv_to_json")
     path_to_json_from_csv = DataInputFromCsv.create_input_json()
     user_input.update({"path_input_file": path_to_json_from_csv})
-    
+
     logging.debug("Accessing script: B0_data_input_json")
     dict_values = data_input.get(user_input)
 
