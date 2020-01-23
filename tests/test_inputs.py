8e0233fa0c67343a3d81118fbc7a431a1e88afbd
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-23 15:35:36 +0100

    Add test for input_path

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -4,21 +4,24 @@ import mock
 
 import src.A0_initialization as initializing
 
+from .constants import REPO_PATH
 
-def test_create_output_path():
-
-    with mock.patch('builtins.input', return_value="yes"):
-        initializing.check_output_directory(os.path.join('test_func'), False)
-        assert os.path.exists('test_func')
+# def test_create_output_path():
+#
+#     with mock.patch('builtins.input', return_value="yes"):
+#         initializing.check_output_directory(os.path.join('test_func'), False)
+#         assert os.path.exists('test_func')
 
 
 def test_check_input_path_posix():
-    folder, file = initializing.check_input_directory(".inputs/working_example.json")
-    assert folder == "inputs"
-    assert file == "working_example.json"
+    if os.name == "posix":
+        folder, file = initializing.check_input_directory(
+            "{}/inputs/working_example.json".format(REPO_PATH)
+        )
+    else:
+        folder, file = initializing.check_input_directory(
+            "{}\\inputs\\working_example.json".format(REPO_PATH)
+        )
 
-
-def test_check_input_path_windows():
-    folder, file = initializing.check_input_directory(".inputs\\working_example.json")
-    assert folder == "inputs"
+    assert folder == os.path.join(REPO_PATH, "inputs")
     assert file == "working_example.json"
