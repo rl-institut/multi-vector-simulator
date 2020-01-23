e781eceb54af4bdee8929a9487456a2f65c27065
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-23 18:48:56 +0100

    Add tests for the csv to json conversion

---
+++ tests/test_input_conversion.py
@@ -0,0 +1,31 @@
+import os
+
+from .constants import REPO_PATH
+
+import src.A1_csv_to_json as load_data_from_csv
+import src.B0_data_input_json as data_input
+from src.A1_csv_to_json import ALLOWED_FILES
+
+elements = os.path.join(REPO_PATH, "inputs", "elements")
+
+
+def test_creation_of_json_file():
+    load_data_from_csv.create_input_json(
+        input_directory=elements, output_filename="test_example_create.json"
+    )
+    assert os.path.exists(os.path.join(elements, "test_example_create.json"))
+
+
+def test_required_fields_are_filled():
+
+    js_file = load_data_from_csv.create_input_json(
+        input_directory=elements, output_filename="test_example_field.json"
+    )
+    js = data_input.load_json(js_file)
+    for k in js.keys():
+        assert k in ALLOWED_FILES
+
+
+def teardown_module():
+    os.remove(os.path.join(elements, "test_example_create.json"))
+    os.remove(os.path.join(elements, "test_example_field.json"))
