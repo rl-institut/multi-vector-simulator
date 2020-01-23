d08b86f5a447326c42a316692f459b7c5070516c
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-23 18:49:59 +0100

    Refactor load json

--- src/B0_data_input_json.py
+++ src/B0_data_input_json.py
@@ -1,12 +1,12 @@
 import json
 
 
-def load_json(user_input):
+def load_json(path_input_file):
     """
 
-    :param user_input:
+    :param path_input_file:
     :return:
     """
-    with open(user_input["path_input_file"]) as json_file:
+    with open(path_input_file) as json_file:
         dict_values = json.load(json_file)
     return dict_values
