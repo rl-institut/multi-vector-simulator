96d4e471a94d484f512782ccc4de2440693936e4
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-23 19:07:18 +0100

    Add two more draft tests of create_json_from_csv

--- tests/test_input_conversion.py
+++ tests/test_input_conversion.py
@@ -9,14 +9,14 @@ from src.A1_csv_to_json import ALLOWED_FILES
 elements = os.path.join(REPO_PATH, "inputs", "elements")
 
 
-def test_creation_of_json_file():
+def test_create_input_json_creation_of_json_file():
     load_data_from_csv.create_input_json(
         input_directory=elements, output_filename="test_example_create.json"
     )
     assert os.path.exists(os.path.join(elements, "test_example_create.json"))
 
 
-def test_required_fields_are_filled():
+def test_create_input_json_required_fields_are_filled():
 
     js_file = load_data_from_csv.create_input_json(
         input_directory=elements, output_filename="test_example_field.json"
@@ -24,6 +24,14 @@ def test_required_fields_are_filled():
     js = data_input.load_json(js_file)
     for k in js.keys():
         assert k in ALLOWED_FILES
+
+
+def test_create_json_from_csv_file_not_exist():
+    pass
+
+
+def test_create_json_from_csv_correct_output():
+    pass
 
 
 def teardown_module():
