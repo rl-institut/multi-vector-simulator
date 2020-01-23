5278dde913e2ddf9d30acf3b1d4efde4d0327883
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-01-23 12:14:33 +0100

    Remove class structure for B0

--- src/B0_data_input_json.py
+++ src/B0_data_input_json.py
@@ -1,10 +1,12 @@
-import pandas as pd
-import pprint as pp
 import json
 
 
-class data_input:
-    def get(user_input):
-        with open(user_input["path_input_file"]) as json_file:
-            dict_values = json.load(json_file)
-        return dict_values
+def load_json(user_input):
+    """
+
+    :param user_input:
+    :return:
+    """
+    with open(user_input["path_input_file"]) as json_file:
+        dict_values = json.load(json_file)
+    return dict_values
