662b88529f8b772bfbb42cf1c8e3845e1e9ae4bc
Pierre Francois <pierre-francois.duc@netplus.ch>
2020-01-27 11:48:51 +0100

    Improve docstrings
    Co-Authored-By: smartie2076 <44204527+smartie2076@users.noreply.github.com>

--- src/B0_data_input_json.py
+++ src/B0_data_input_json.py
@@ -2,10 +2,10 @@ import json
 
 
 def load_json(path_input_file):
-    """
+    """Opens and reads json input file and parses it to dict of input parameters.
 
     :param path_input_file:
-    :return:
+    :return: dict of all input parameters
     """
     with open(path_input_file) as json_file:
         dict_values = json.load(json_file)
