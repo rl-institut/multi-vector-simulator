b37834355e4ea16d14b6da15a8e7bb643bfa1e65
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-01-30 15:02:40 +0100

    Test a normal execution of the program
    Mock patch is used to mock command line arguments

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,9 +1,24 @@
-import pytest
 import os
+import mock
+import argparse
+import shutil
 
 from mvs_eland_tool.mvs_eland_tool import main
 
+OUTPUT_PATH = os.path.join(".", "tests", "MVS_outputs_simulation")
 
-def test_run_smoothly():
-    # Ensure that code does not terminate
+
+def setup_module():
+    if os.path.exists(OUTPUT_PATH):
+        shutil.rmtree(OUTPUT_PATH, ignore_errors=True)
+
+
+@mock.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace())
+def test_run_smoothly(mock_args):
+
+    main(path_output_folder=OUTPUT_PATH)
     assert 1 == 1
+
+
+def teardown_module():
+    shutil.rmtree(OUTPUT_PATH, ignore_errors=True)
