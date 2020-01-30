706b866b49357acd97225206e512da037ca59edd
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-01-30 15:19:26 +0100

    Fix the test
    Fix #108

--- mvs_eland_tool/mvs_eland_tool.py
+++ mvs_eland_tool/mvs_eland_tool.py
@@ -72,7 +72,7 @@ def main(**kwargs):
     #    # todo: is user input completely used?
     #    dict_values = data_input.load_json(user_input["path_input_file"])
 
-    if not kwargs["path_input_file"].endswith("json"):
+    if not user_input["path_input_file"].endswith("json"):
         logging.debug("Accessing script: A1_csv_to_json")
         path_to_json_from_csv = load_data_from_csv.create_input_json()
         user_input.update({"path_input_file": path_to_json_from_csv})
