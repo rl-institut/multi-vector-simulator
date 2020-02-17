defe2d6d3f26a19057a262ae7608f1d71925b9cf
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2020-02-17 17:37:22 +0100

    path to sequences changed

--- src/C0_data_processing.py
+++ src/C0_data_processing.py
@@ -934,7 +934,7 @@ def receive_timeseries_from_csv(settings, dict_asset, type):
         header = dict_asset[type]["value"]["header"]
         unit = dict_asset[type]["unit"]
 
-    file_path = os.path.join(settings["path_input_folder"], file_name)
+    file_path = os.path.join(settings["path_input_sequences"], file_name)
     verify.lookup_file(file_path, dict_asset["label"])
 
     data_set = pd.read_csv(file_path, sep=";")
@@ -1091,7 +1091,7 @@ def get_timeseries_multiple_flows(settings, dict_asset, file_name, header):
     -------
 
     """
-    file_path = os.path.join(settings["path_input_folder"], file_name)
+    file_path = os.path.join(settings["path_input_sequences"], file_name)
     verify.lookup_file(file_path, dict_asset["label"])
 
     data_set = pd.read_csv(file_path, sep=";")
