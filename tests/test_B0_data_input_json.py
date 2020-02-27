e9042dc9e350a9c4bd5788cfb4d268af17b02031
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-02-27 17:26:34 +0100

    Rename test files
    The test modules are renamed after the src modules they refer to

--- tests/test_B0_data_input_json.py
+++ tests/test_B0_data_input_json.py
@@ -9,13 +9,6 @@ from src.A1_csv_to_json import ALLOWED_FILES
 elements = os.path.join(REPO_PATH, "tests", "inputs", "csv")
 
 
-def test_create_input_json_creation_of_json_file():
-    load_data_from_csv.create_input_json(
-        input_directory=elements, output_filename="test_example_create.json"
-    )
-    assert os.path.exists(os.path.join(elements, "test_example_create.json"))
-
-
 def test_create_input_json_required_fields_are_filled():
 
     js_file = load_data_from_csv.create_input_json(
