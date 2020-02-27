05db2ed1fd3d30176f18baa4db32f7f5ba6f4da2
smartie2076 <martha.hoffmann@rl-institut.de>
2020-02-27 16:49:47 +0100

    Add test_A0 file and docstring

---
+++ tests/test_A0_initialization.py
@@ -0,0 +1,13 @@
+"
+Module A0_initialization should define the most basic settings of the MVS simulation. This includes
+- Executing the MVS from terminal
+   - Using json input file
+   - Using csv input files
+   - Optional: Force folder overwrite
+   - Optional: Display logging in terminal
+- Set default values for MVS execution
+- Check that all necessary (and called) files exist
+- Create output directory
+- Display welcome message with current version number
+- Define logging depth
+"
