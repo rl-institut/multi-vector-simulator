12870e20a4ba3ff3899325793a80ddfef4fdfa3e
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-03-02 18:20:02 +0100

    Create module to centralize global constants

---
+++ src/constants.py
@@ -0,0 +1,34 @@
+import os
+
+# path to the root of this repository (assumes this file is in src folder)
+REPO_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INPUT_FOLDER = "inputs"
+# name of the folder containing mvs model described by .csv files
+CSV_ELEMENTS = "csv_elements"
+# name of the json file which should be present in the input folder if option -i json was chosen
+JSON_FNAME = "mvs_config.json"
+# name of the json file which is should be created in the input folder if option -i csv was chosen
+CSV_FNAME = "mvs_csv_config.json"
+# list of csv filename which must be present within the CSV_ELEMENTS folder
+REQUIRED_CSV_FILES = (
+    "fixcost",
+    "simulation_settings",
+    "project_data",
+    "economic_data",
+    "energyConversion",
+    "energyProduction",
+    "energyStorage",
+    "energyProviders",
+    "energyConsumption",
+)
+# name of the folder containing timeseries described by .csv files
+SEQUENCES = "sequences"
+# name of the folder containing the output of the simulation
+OUTPUT_FOLDER = "MVS_outputs"
+# name of the folder containing the copied content of the input folder within the output folder
+INPUTS_COPY = INPUT_FOLDER
+
+# default paths to input, output and sequences folders
+DEFAULT_INPUT_PATH = os.path.join(REPO_PATH, INPUT_FOLDER)
+DEFAULT_OUTPUT_PATH = os.path.join(REPO_PATH, OUTPUT_FOLDER)
+DEFAULT_SEQUENCES_PATH = os.path.join(REPO_PATH, INPUT_FOLDER, SEQUENCES)
