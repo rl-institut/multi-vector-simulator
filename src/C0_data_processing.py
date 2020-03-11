7efb32df6ba9af0a2ceb40d61e639f7cddf7776e
smartie2076 <gitMA18rli>
2020-03-11 12:22:23 +0100

    Formatting with blacks

--- src/C0_data_processing.py
+++ src/C0_data_processing.py
@@ -1060,7 +1060,6 @@ def receive_timeseries_from_csv(settings, dict_asset, type):
     return
 
 
-
 def plot_input_timeseries(user_input, timeseries, asset_name, column_head):
     logging.info("Creating plots for asset %s's parameter %s", asset_name, column_head)
     fig, axes = plt.subplots(nrows=1, figsize=(16 / 2.54, 10 / 2.54 / 2))
