34a2c17fa9df275ca6b1c72ed672b9c1a3fa277b
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-03-13 00:58:06 +0100

    Bump up version number

--- mvs_eland_tool/mvs_eland_tool.py
+++ mvs_eland_tool/mvs_eland_tool.py
@@ -68,7 +68,7 @@ def main(**kwargs):
 
     """
     version = (
-        "0.1.1"  # update_me Versioning scheme: Major release.Minor release.Patches
+        "0.2.0"  # update_me Versioning scheme: Major release.Minor release.Patches
     )
     date = "25.11.2019"  # update_me Update date
 
