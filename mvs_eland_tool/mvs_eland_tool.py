0f06169e57b767cc711bc6908a393b15c8d72371
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-03-13 08:01:11 +0100

    Change release date

--- mvs_eland_tool/mvs_eland_tool.py
+++ mvs_eland_tool/mvs_eland_tool.py
@@ -70,7 +70,7 @@ def main(**kwargs):
     version = (
         "0.2.0"  # update_me Versioning scheme: Major release.Minor release.Patches
     )
-    date = "25.11.2019"  # update_me Update date
+    date = "2020-03-13"  # update_me Update date
 
     welcome_text = (
         "\n \n Multi-Vector Simulation Tool (MVS) V"
