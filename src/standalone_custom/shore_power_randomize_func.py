822f6b28b6007d25e65baff6e55306ed7dc48a2e
SabineH <sabine.haas@rl-institut.de>
2020-03-18 11:14:18 +0100

    Improve randomize shore power and implement dock availability

--- src/standalone_custom/shore_power_randomize_func.py
+++ src/standalone_custom/shore_power_randomize_func.py
@@ -7,67 +7,75 @@ def randomize_shore_power(index, shore_power, filename=None, docks=1):
     # Spalten in shore_power fassen Andock-Zeiten von Schiffen zusammen, die nicht gleichzeitig laden können.
     # Schiffe der versch. Spalten können gleichzeitig geladne werdne, wenn mehr als ein Dock. `docks`=1, 2, 3
     # todo einführen: Laden zweier Schiffe soll mind. einmal überlappen FRAGE: heißt überlappen kompl. Dauer des einen Schiffes oder mind. ein Zeitschritt?
-    total_shore_power = pd.Series([0 for i in range(0, len(index))], index=index)
+    total_shore_power = pd.DataFrame([0 for i in range(0, len(index))], index=index, columns=['count'])
 
     for column in shore_power.columns:
         correct_combination = False
         while correct_combination != True:
-            print("Get shore power times for: ", column, " docking time")
+            print("Get shore power times for: ", column, " docking time")  # todo logging
             time_of_events = pd.Series(
                 [shore_power[column]["Power"] for i in range(0, len(index))], index=index
-            ).sample(
+            ).reset_index()['index'].sample(
                 n=shore_power[column]["Number"], replace=False  # number of events
             )
             correct_combination = True
-            number = 0
-            for i in time_of_events.index:
-                if correct_combination == True:
-                    # check if period i collides with one of the other periods
-                    for j in time_of_events.index:
-                        if i != j:
-                            if j in pd.date_range(
-                                    start=i,
-                                    end=i
-                                        + pd.DateOffset(
-                                        hours=int(shore_power[column]["Duration"])),
-                                    freq="h",
-                            ):   # note: 1 h mehr, da Zeit zum an und abfahren nötig
-                                correct_combination = False
 
-                    if correct_combination == True:
-                        # write power to total_shore_power if there is an available
-                        # dock and if time period does not exceed the date time
-                        for hour in range(0, shore_power[column]["Duration"]):
-                            try:
-                                power = total_shore_power[
-                                    i + pd.DateOffset(hours=hour)]
-                            except KeyError:
-                                # time period exceeds date time --> not possible
-                                power = None
-                            if power in [0, 500]: # todo  abh. v. docks
-                                # dock is available
-                                total_shore_power[
-                                    i + pd.DateOffset(hours=hour)
-                                ] += shore_power[column]["Power"]
-                            else:
-                                # dock not available or time period exceeds index
-                                correct_combination = False
-                                continue
-                    else:
-                        continue
-                else:
-                    continue
+            # check if any period collides with one of the other periods  # todo note: no time between charging
+            # check, as well, whether periods stay within `times` # todo or index
+            time_periods = time_of_events.apply(
+                lambda x: pd.date_range(start=x, end=x + pd.DateOffset(
+                    hours=int(shore_power[column]["Duration"] -1)),
+                                        freq="h")).values
+            joined_periods = time_periods[0]
+            for i in range(1, len(time_periods)):
+                joined_periods = joined_periods.join(time_periods[i],
+                                                     how='outer')
 
-                if correct_combination == True:
-                    number += 1
-                    print(
-                        "Valid random event ",
-                        number,
-                        " of total ",
-                        shore_power[column]["Number"],
-                    )
-                else:
-                    continue
+            expected_length = (shore_power[column]["Duration"]) * len(
+                time_of_events)
+            if len(joined_periods) != expected_length or \
+                    not index.contains(joined_periods[-1]):
+                # try a new combination of "start" time steps
+                correct_combination = False
+                continue
+
+            # write power to total_shore_power if there is an available dock #
+
+            # check doc availability
+            availability = total_shore_power['count'].loc[
+                joined_periods].apply(lambda x: True if x < docks else False)
+            if not availability.all():
+                # try a new combination of "start" time steps
+                correct_combination = False
+                continue
+
+            # temporary column
+            total_shore_power[f'temp_{column}'] = 0
+            total_shore_power.loc[joined_periods, f'temp_{column}'] = shore_power[column]['Power']
+
+            # update count of docks
+            total_shore_power.loc[joined_periods, 'count'] += 1
+
+            # just to be sure: check whether expected sum was written
+            expected_sum = (shore_power[column]['Power'] *
+                            shore_power[column]['Duration'] *
+                            shore_power[column]['Number'])
+            if total_shore_power[f'temp_{column}'].sum() != expected_sum:
+                # delete temporary column and try a new combination of "start"
+                # time steps
+                total_shore_power.drop(f'temp_{column}', axis=1, inplace=True)
+                correct_combination = False
+                continue
+
+    # some information - to be extended if interesting
+    max_docks_used = max(total_shore_power['count'])
+    hours_without_docking = total_shore_power.loc[total_shore_power['count'] == 0.0].index
+    amount_h_without_docking = len(hours_without_docking)
+
+    # sum up shore power
+    total_shore_power.drop('count', axis=1, inplace=True)
+    total_shore_power = total_shore_power.sum(axis=1)
+
     # todo add header to time series and maybe delete index
     total_shore_power.to_csv("demand_shore_power.csv")
     return total_shore_power
