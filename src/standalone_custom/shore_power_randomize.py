3dc821847959bcfb36a84fa4d07422753dea17f0
SabineH <sabine.haas@rl-institut.de>
2020-03-18 11:57:56 +0100

    Apply black

--- src/standalone_custom/shore_power_randomize.py
+++ src/standalone_custom/shore_power_randomize.py
@@ -4,9 +4,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def randomize_shore_power(times, shore_power,
-                          filename="randomized_shore_power_demand.csv",
-                          docks=1):
+def randomize_shore_power(
+    times, shore_power, filename="randomized_shore_power_demand.csv", docks=1
+):
     r"""
     Creates randomized shore power time series.
 
@@ -41,35 +41,45 @@ def randomize_shore_power(times, shore_power,
       be split into several functions.
 
     """
-    total_shore_power = pd.DataFrame([0 for i in range(0, len(times))], index=times, columns=['count'])
+    total_shore_power = pd.DataFrame(
+        [0 for i in range(0, len(times))], index=times, columns=["count"]
+    )
 
     for column in shore_power.columns:
         correct_combination = False
         while correct_combination != True:
             logging.info(f"Get shore power times for ship type: {column}")
             # Get random time steps from time series
-            time_of_events = pd.Series(
-                [shore_power[column]["Power"] for i in range(0, len(times))], index=times
-            ).reset_index()['index'].sample(
-                n=shore_power[column]["Number"],  # number of events
-                replace=False)
+            time_of_events = (
+                pd.Series(
+                    [shore_power[column]["Power"] for i in range(0, len(times))],
+                    index=times,
+                )
+                .reset_index()["index"]
+                .sample(
+                    n=shore_power[column]["Number"], replace=False  # number of events
+                )
+            )
             correct_combination = True
 
             # check if any period collides with one of the other periods
             # check, as well, whether periods stay within `times`
             time_periods = time_of_events.apply(
-                lambda x: pd.date_range(start=x, end=x + pd.DateOffset(
-                    hours=int(shore_power[column]["Duration"] -1)),
-                                        freq="h")).values
+                lambda x: pd.date_range(
+                    start=x,
+                    end=x
+                    + pd.DateOffset(hours=int(shore_power[column]["Duration"] - 1)),
+                    freq="h",
+                )
+            ).values
             joined_periods = time_periods[0]
             for i in range(1, len(time_periods)):
-                joined_periods = joined_periods.join(time_periods[i],
-                                                     how='outer')
+                joined_periods = joined_periods.join(time_periods[i], how="outer")
 
-            expected_length = (shore_power[column]["Duration"]) * len(
-                time_of_events)
-            if (len(joined_periods) != expected_length or
-                    not times.contains(joined_periods[-1])):
+            expected_length = (shore_power[column]["Duration"]) * len(time_of_events)
+            if len(joined_periods) != expected_length or not times.contains(
+                joined_periods[-1]
+            ):
                 # try a new combination of "start" time steps
                 correct_combination = False
                 continue
@@ -77,39 +87,48 @@ def randomize_shore_power(times, shore_power,
             # write power to total_shore_power if there is an available dock #
 
             # check doc availability
-            availability = total_shore_power['count'].loc[
-                joined_periods].apply(lambda x: True if x < docks else False)
+            availability = (
+                total_shore_power["count"]
+                .loc[joined_periods]
+                .apply(lambda x: True if x < docks else False)
+            )
             if not availability.all():
                 # try a new combination of "start" time steps
                 correct_combination = False
                 continue
 
             # temporary column for charging power
-            total_shore_power[f'temp_{column}'] = 0
-            total_shore_power.loc[joined_periods, f'temp_{column}'] = shore_power[column]['Power']
+            total_shore_power[f"temp_{column}"] = 0
+            total_shore_power.loc[joined_periods, f"temp_{column}"] = shore_power[
+                column
+            ]["Power"]
 
             # update count of docks
-            total_shore_power.loc[joined_periods, 'count'] += 1
+            total_shore_power.loc[joined_periods, "count"] += 1
 
             # just to be sure:
             # check whether expected sum was written to `total_shore_power`
-            expected_sum = (shore_power[column]['Power'] *
-                            shore_power[column]['Duration'] *
-                            shore_power[column]['Number'])
-            if total_shore_power[f'temp_{column}'].sum() != expected_sum:
+            expected_sum = (
+                shore_power[column]["Power"]
+                * shore_power[column]["Duration"]
+                * shore_power[column]["Number"]
+            )
+            if total_shore_power[f"temp_{column}"].sum() != expected_sum:
                 # delete temporary column and try a new combination of "start"
                 # time steps
-                total_shore_power.drop(f'temp_{column}', axis=1, inplace=True)
+                total_shore_power.drop(f"temp_{column}", axis=1, inplace=True)
                 correct_combination = False
                 continue
 
     # some information - to be extended if interesting
-    max_docks_used = max(total_shore_power['count'])
-    hours_without_docking = total_shore_power.loc[total_shore_power['count'] == 0.0].index
+    max_docks_used = max(total_shore_power["count"])
+    hours_without_docking = total_shore_power.loc[
+        total_shore_power["count"] == 0.0
+    ].index
     amount_h_without_docking = len(hours_without_docking)
 
     # sum up shore power
-    total_shore_power.drop('count', axis=1, inplace=True)
+    total_shore_power.drop("count", axis=1, inplace=True)
     total_shore_power = total_shore_power.sum(axis=1)
 
     logging.info(f"Write shore power time series to file {filename}.")
--- src/standalone_custom/test_shore_power_randomize.py
+++ src/standalone_custom/test_shore_power_randomize.py
@@ -3,10 +3,11 @@ import pytest
 
 from src.standalone_custom.shore_power_randomize import randomize_shore_power
 
+
 def test_randomize_shore_power():
     shore_power_df = {
         "1000": {"Number": 1, "Duration": 2, "Power": 500},
-        "400": {"Number": 1, "Duration": 4, "Power": 100}
+        "400": {"Number": 1, "Duration": 4, "Power": 100},
     }
     shore_power_df = pd.DataFrame(shore_power_df)
 
