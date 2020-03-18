742d3a4408623f7d9371e8ebc92baf110a57f5c6
SabineH <sabine.haas@rl-institut.de>
2020-03-18 11:41:36 +0100

    Add docstring and comments

--- src/standalone_custom/shore_power_randomize_func.py
+++ src/standalone_custom/shore_power_randomize_func.py
@@ -3,28 +3,57 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def randomize_shore_power(times, shore_power, filename="demand_shore_power.csv", docks=1):
-    # fuer docstring
-    # Annahme bzw. Anforderung Daten:
-    # Spalten in shore_power fassen Andock-Zeiten von Schiffen zusammen, die nicht gleichzeitig laden können.
-    # Schiffe der versch. Spalten können gleichzeitig geladne werdne, wenn mehr als ein Dock. `docks`=1, 2, 3
-    # todo einführen: Laden zweier Schiffe soll mind. einmal überlappen FRAGE: heißt überlappen kompl. Dauer des einen Schiffes oder mind. ein Zeitschritt?
 
+def randomize_shore_power(times, shore_power,
+                          filename="demand_shore_power.csv", docks=1):
+    r"""
+    Creates randomized shore power time series.
+
+    times : pandas.DatetimeIndex
+        Time period for which randomized shore power time series is created.
+    shore_power : pandas.DataFrame
+        Specification of shore power with row indices 'Duration' for the
+        duration of a docking period of a ship, 'Number' showing the amount of
+        docking events during `times` and 'Power` specifying the power of the
+        respective ship. Add one column for each group of ships that share
+        the same power and docking duration (name free to choose). It is
+        assumed that ships of one column cannot occupy a dock at the same time,
+        ships of different columns can though, if `docks` is greater than one.
+    filename : str
+        File name including path for saving the shore power time series.
+        Default: "demand_shore_power.csv".
+    docks : int
+        Amount of docks that are available simultaneously. Default: 1.
+
+    Returns
+    -------
+    total_shore_power : pandas.Series
+        Randomized shore power time series.
+
+    Todo
+    -----
+    * Adapt format of `total_shore_power` for MVS. (f.e. header, index ...)
+    * Now there's now time step between two ships charging. There could be one
+      one hour in between for docking and undocking.
+    * Possible feature: Maximum amount of periods in which all docks are used.
+
+    """
     total_shore_power = pd.DataFrame([0 for i in range(0, len(times))], index=times, columns=['count'])
 
     for column in shore_power.columns:
         correct_combination = False
         while correct_combination != True:
             logging.info(f"Get shore power times for: {column} docking time.")
+            # Get random time steps from time series
             time_of_events = pd.Series(
                 [shore_power[column]["Power"] for i in range(0, len(times))], index=times
             ).reset_index()['index'].sample(
-                n=shore_power[column]["Number"], replace=False  # number of events
-            )
+                n=shore_power[column]["Number"],  # number of events
+                replace=False)
             correct_combination = True
 
-            # check if any period collides with one of the other periods  # todo note: no time between charging
-            # check, as well, whether periods stay within `times` # todo or index
+            # check if any period collides with one of the other periods
+            # check, as well, whether periods stay within `times`
             time_periods = time_of_events.apply(
                 lambda x: pd.date_range(start=x, end=x + pd.DateOffset(
                     hours=int(shore_power[column]["Duration"] -1)),
@@ -52,14 +81,15 @@ def randomize_shore_power(times, shore_power, filename="demand_shore_power.csv",
                 correct_combination = False
                 continue
 
-            # temporary column
+            # temporary column for charging power
             total_shore_power[f'temp_{column}'] = 0
             total_shore_power.loc[joined_periods, f'temp_{column}'] = shore_power[column]['Power']
 
             # update count of docks
             total_shore_power.loc[joined_periods, 'count'] += 1
 
-            # just to be sure: check whether expected sum was written
+            # just to be sure:
+            # check whether expected sum was written to `total_shore_power`
             expected_sum = (shore_power[column]['Power'] *
                             shore_power[column]['Duration'] *
                             shore_power[column]['Number'])
