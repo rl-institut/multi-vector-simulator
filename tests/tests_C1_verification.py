7196185a808f094ae9ff5494373298825992aa28
SabineH <sabine.haas@rl-institut.de>
2020-03-20 12:09:32 +0100

    Add test function names

---
+++ tests/tests_C1_verification.py
@@ -0,0 +1,25 @@
+import os
+
+from .constants import REPO_PATH
+
+import src.C1_verification as C1
+
+
+def test_lookup_file_existing_file():
+    pass
+
+
+def test_lookup_file_non_existing_file():
+    pass
+
+
+# def test_check_input_values():
+#     pass
+#     # todo note: function is not used so far
+
+
+# def test_all_valid_intervals():
+#     pass
+#     # todo note: function is not used so far
+
+
