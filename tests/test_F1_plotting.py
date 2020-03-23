0adb900c5bc5515bb9001efccbe8636b071959dc
smartie2076 <44204527+smartie2076@users.noreply.github.com>
2020-03-23 17:10:49 +0100

    Add test names F1

---
+++ tests/test_F1_plotting.py
@@ -0,0 +1,11 @@
+def test_if_plot_of_all_energy_flows_for_all_sectors_are_stored:
+    assert False
+
+def test_if_bar_charts_of_costs_are_stored:
+    assert False
+    
+def test_if_bar_charts_of_capacities_are_stored:
+    assert False
+    
+def test_if_networkx_graph_is_stored:
+    assert False
