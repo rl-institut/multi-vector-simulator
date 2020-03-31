a974d0ceab37d38317eeaf894173f1a1e87b5d16
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-03-31 11:47:46 +0200

    Update C2 functions tests as per comments

--- tests/tests_C2_economic_functions.py
+++ tests/tests_C2_economic_functions.py
@@ -4,8 +4,11 @@ project_life = 20
 wacc = 0.1
 investment_t0 = 220000
 tax = 0.15
-lifetime = [20, 15]
+lifetime = [20, 15, 35]
 present_value = 295000
+crf = 0.12
+annuity = 35400
+annuity_factor = 1 / 0.12
 fuel_keys = {
     "fuel_price": 1.3,
     "fuel_price_change_annual": 0,
@@ -32,21 +35,36 @@ def test_crf():
     assert CRF == (0.1 * (1 + 0.1) ** 20) / ((1 + 0.1) ** 20 - 1)
 
 
-def test_capex_from_investment(lifetime):
+def test_capex_from_investment_lifetime_equals_project_life():
     """
 
-    Tests whether the MVS is correctly calculating the capital expenditure of the project
+    Tests whether the MVS is correctly calculating the capital expenditure of the project if the lifetime is equal to project_life
     """
-    for years in lifetime:
-        if years == project_life:
-            CAPEX = e_functions.capex_from_investment(investment_t0, years, project_life,wacc,tax)
-            assert CAPEX == 220000*(1+0.15)
-        else:
-            CAPEX = e_functions.capex_from_investment(investment_t0, years, project_life, wacc, tax)
-            first_investment = 220000*(1+0.15)
-            assert CAPEX == first_investment + first_investment / (1 + 0.1) ** (15 * 1) + first_investment \
-                   + first_investment / (1 + 0.1) ** (15 * 1) + first_investment / (1 + 0.1) ** (15 * 2) \
-                   - ((first_investment / ((1 + 0.1) ** 15)) / 15) * (2 * 15 - 20)
+    CAPEX = e_functions.capex_from_investment(investment_t0, lifetime[0], project_life, wacc, tax)
+    assert CAPEX == 220000 * (1 + 0.15)
+
+
+def test_capex_from_investment_lifetime_smaller_than_project_life():
+    """
+
+    Tests whether the MVS is correctly calculating the capital expenditure of the project if the lifetime is smaller than project_life
+    """
+    CAPEX = e_functions.capex_from_investment(investment_t0, lifetime[1], project_life, wacc, tax)
+    first_investment = 220000 * (1 + 0.15)
+    assert CAPEX == first_investment + first_investment / (1 + 0.1) ** (1 * 15) + first_investment \
+           + first_investment / (1 + 0.1) ** (1 * 15) + first_investment / (1 + 0.1) ** (2 * 15) \
+           - ((first_investment / ((1 + 0.1) ** ((2 - 1) * 15))) / 15) * (2 * 15 - 20)
+
+
+def test_capex_from_investment_lifetime_bigger_than_project_life():
+    """
+
+    Tests whether the MVS is correctly calculating the capital expenditure of the project if the lifetime is bigger than project_life
+    """
+    CAPEX = e_functions.capex_from_investment(investment_t0, lifetime[2], project_life, wacc, tax)
+    first_investment = 220000 * (1 + 0.15)
+    assert CAPEX == first_investment + first_investment / (1 + 0.1) ** (1 * 35) \
+           - ((first_investment / ((1 + 0.1) ** ((1 - 1) * 35))) / 35) * (1 * 35 - 20)
 
 
 def test_annuity():
@@ -54,9 +72,8 @@ def test_annuity():
 
     Tests whether the MVS is correctly calculating the annuity value
     """
-    CRF = e_functions.crf(project_life, wacc)
-    A = e_functions.annuity(present_value,CRF)
-    assert A == 295000 * ((0.1 * (1 + 0.1) ** 20) / ((1 + 0.1) ** 20 - 1))
+    A = e_functions.annuity(present_value, crf)
+    assert A == 295000 * 0.12
 
 
 def test_present_value_from_annuity():
@@ -64,22 +81,20 @@ def test_present_value_from_annuity():
 
     Tests whether the MVS is correctly calculating the present value
     """
-    A = e_functions.annuity(present_value, CRF)
-    AF = e_functions.annuity_factor(project_life, wacc)
-    PV_from_annuity = e_functions.present_value_from_annuity(A, AF)
-    assert PV_from_annuity == 295000 * ((0.1 * (1 + 0.1) ** 20) / ((1 + 0.1) ** 20 - 1)) * (1 / 0.1 - 1 / (0.1 * (1 + 0.1) ** 20))
+    PV_from_annuity = e_functions.present_value_from_annuity(annuity, annuity_factor)
+    assert PV_from_annuity == 35400 * (1 / 0.12)
 
 
-def test_fuel_price_present_value(economics):
+def test_fuel_price_present_value_without_fuel_price_change(economics):
     """
 
-    Tests whether the MVS is correctly calculating the present value of the fuel price over the lifetime of the project
+    Tests whether the MVS is correctly calculating the present value of the fuel price over the lifetime of the project without fuel price change
     """
     PV_fuel_price = e_functions.fuel_price_present_value(fuel_keys)
     assert PV_fuel_price == 1.3
 
-    fuel_keys["fuel_price_change_annual"] = 0.008
-    fuel_keys["crf"] = e_functions.crf(project_life, wacc)
-    PV_fuel_price = e_functions.fuel_price_present_value(fuel_keys)
+    # fuel_keys["fuel_price_change_annual"] = 0.008
+    # fuel_keys["crf"] = e_functions.crf(project_life, wacc)
+    # PV_fuel_price = e_functions.fuel_price_present_value(fuel_keys)
     # still not sure how not to use float number in assert
-    assert PV_fuel_price == 13.06049955 * ((0.1 * (1 + 0.1) ** 20) / ((1 + 0.1) ** 20 - 1))
+    # assert PV_fuel_price == 13.06049955 * ((0.1 * (1 + 0.1) ** 20) / ((1 + 0.1) ** 20 - 1))
