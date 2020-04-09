2a00224e2b15c6bd707f18efab24b016fce5b617
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-04-09 16:44:25 +0200

    Reformat tests/constants.py

--- tests/constants.py
+++ tests/constants.py
@@ -1,17 +1,9 @@
 import os
 
 # import constants from src
-from src.constants import INPUT_FOLDER
+from src.constants import INPUT_FOLDER, JSON_FNAME, CSV_ELEMENTS, INPUTS_COPY
 
-from src.constants import JSON_FNAME
+TEST_REPO_PATH = os.path.dirname(__file__)
 
-from src.constants import REPO_PATH
-
-# name of the folder containing mvs model described by ".csv" files
-CSV_ELEMENTS = "csv_elements"
-# name of the folder containing the copied content of the input folder within the output folder
-INPUTS_COPY = "inputs"
-
-TEST_REPO_PATH = os.path.dirname((__file__))
-
+CSV_PATH = os.path.join(TEST_REPO_PATH, INPUT_FOLDER, CSV_ELEMENTS)
 JSON_PATH = os.path.join(TEST_REPO_PATH, INPUT_FOLDER, JSON_FNAME)
