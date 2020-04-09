9e11fd078c13f0dc3d5847ad530bb8bd77714db3
SabineH <sabine.haas@rl-institut.de>
2020-04-09 15:45:29 +0200

    Imports to top

--- tests/constants.py
+++ tests/constants.py
@@ -1,14 +1,15 @@
 import os
 
+# import constants from src
+from src.constants import INPUT_FOLDER
+
+from src.constants import JSON_FNAME
+
 # name of the folder containing mvs model described by ".csv" files
 CSV_ELEMENTS = "csv_elements"
 # name of the folder containing the copied content of the input folder within the output folder
 INPUTS_COPY = "inputs"
 
 REPO_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-from src.constants import INPUT_FOLDER
-
-from src.constants import JSON_FNAME
 
 JSON_PATH = os.path.join(REPO_PATH, INPUT_FOLDER, JSON_FNAME)
