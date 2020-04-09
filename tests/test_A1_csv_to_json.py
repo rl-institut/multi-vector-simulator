87ec3946690b15f0f4e9367a67b613805ccfe88b
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-04-09 16:46:15 +0200

    Copy tests from B0

--- tests/test_A1_csv_to_json.py
+++ tests/test_A1_csv_to_json.py
@@ -1,18 +1,36 @@
 import os
 
-from src.constants import REPO_PATH, INPUT_FOLDER, CSV_ELEMENTS
+from .constants import CSV_PATH
 
+import src.B0_data_input_json as data_input
 import src.A1_csv_to_json as load_data_from_csv
-
-elements = os.path.join(REPO_PATH, "tests", INPUT_FOLDER, CSV_ELEMENTS)
 
 
 def test_create_input_json_creation_of_json_file():
     load_data_from_csv.create_input_json(
-        input_directory=elements, output_filename="test_example_create.json"
+        input_directory=CSV_PATH, output_filename="test_example_create.json"
     )
-    assert os.path.exists(os.path.join(elements, "test_example_create.json"))
+    assert os.path.exists(os.path.join(CSV_PATH, "test_example_create.json"))
+
+
+def test_create_input_json_required_fields_are_filled():
+    pass
+    #
+    # js_file = load_data_from_csv.create_input_json(
+    #     input_directory=CSV_PATH, output_filename="test_example_field.json"
+    # )
+    # js = data_input.load_json(js_file)
+    # for k in js.keys():
+    #     assert k in load_data_from_csv.REQUIRED_FILES
+
+
+def test_create_json_from_csv_file_not_exist():
+    pass
+
+
+def test_create_json_from_csv_correct_output():
+    pass
 
 
 def teardown_module():
-    os.remove(os.path.join(elements, "test_example_create.json"))
+    os.remove(os.path.join(CSV_PATH, "test_example_create.json"))
