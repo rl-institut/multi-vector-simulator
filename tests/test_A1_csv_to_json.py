e8d8a7df319046be9a4486a69751100b0f3d0dea
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-04-16 18:47:53 +0200

    Add test for each of the separator

--- tests/test_A1_csv_to_json.py
+++ tests/test_A1_csv_to_json.py
@@ -5,6 +5,8 @@ from src.constants import REPO_PATH, INPUT_FOLDER, CSV_ELEMENTS
 import src.A1_csv_to_json as load_data_from_csv
 
 elements = os.path.join(REPO_PATH, "tests", INPUT_FOLDER, CSV_ELEMENTS)
+CSV_TEST = os.path.join(REPO_PATH, "tests", "test_data")
+CSV_PARAMETERS = ["param1", "param2"]
 
 
 def test_create_input_json_creation_of_json_file():
@@ -12,6 +14,30 @@ def test_create_input_json_creation_of_json_file():
         input_directory=elements, output_filename="test_example_create.json"
     )
     assert os.path.exists(os.path.join(elements, "test_example_create.json"))
+
+
+def test_create_json_from_csv_with_comma_separated_csv():
+    d = load_data_from_csv.create_json_from_csv(
+        CSV_TEST, "csv_comma", CSV_PARAMETERS, storage=False
+    )
+
+    assert d == {
+        "csv_comma": {
+            "col1": {"param1": "val11", "param2": {"value": 21, "unit": "factor"}}
+        }
+    }
+
+
+def test_create_json_from_csv_with_semicolumn_separated_csv():
+    d = load_data_from_csv.create_json_from_csv(
+        CSV_TEST, "csv_semicolumn", CSV_PARAMETERS, storage=False
+    )
+
+    assert d == {
+        "csv_semicolumn": {
+            "col1": {"param1": "val11", "param2": {"value": 21, "unit": "factor"}}
+        }
+    }
 
 
 def teardown_module():
