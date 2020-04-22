2b27db3df70b1002cf50cbb0ee700e9c4791226d
smartie2076 <martha.hoffmann@rl-institut.de>
2020-04-22 17:18:35 +0200

    Revert "Function + test to determine sector-specific renewable share"
    This reverts commit d5eb4ab9

--- src/E3_indicator_calculation.py
+++ src/E3_indicator_calculation.py
@@ -32,56 +32,32 @@ def total_demand_each_sector(dict_values):
     """
     return
 
-def total_renewable_and_non_renewable_energy_origin(dict_values):
+def total_renewable_energy(dict_values):
     """
     Identifies all renewable generation assets and summs up their total generation to total renewable generation
     :param dict_values: dict with all project input data and results up to E0
-    :return: Updated dict_values with total internal/overall renewable and non-renewable energy origin
+    :return: Updated dict_values with list of all renewable sources and their total generation
     """
-    renewable_origin = {}
-    non_renewable_origin = {}
+    renewable_generation = {}
     for sector in dict_values["project_data"]["sectors"]:
-        renewable_origin.update({sector: 0})
-        non_renewable_origin.update({sector: 0})
+        renewable_generation.update({sector: 0})
 
     for asset in dict_values["energyProduction"]:
-        if "renewableAsset" in dict_values["energyProduction"][asset]:
+        if "renewableAsset" in dict_values["energyProduction"][asset] \
+                and dict_values["energyProduction"][asset]["renewableAsset"]["value"] is True:
             sector = dict_values["energyProduction"][asset]["energyVector"]
-            if dict_values["energyProduction"][asset]["renewableAsset"]["value"] is True:
-                renewable_origin[sector] += dict_values["energyProduction"][asset]["total_flow"]["value"]
-            else:
-                non_renewable_origin[sector] += dict_values["energyProduction"][asset]["total_flow"]["value"]
+            renewable_generation[sector] += dict_values["energyProduction"][asset]["total_flow"]["value"]
 
-
-    dict_values["kpi"]["scalars"].update({"Total internal renewable generation": renewable_origin.copy(),
-                                          "Total internal non-renewable generation": non_renewable_origin.copy()})
+    dict_values["kpi"]["scalars"].update({"Total internal renewable generation": renewable_generation.copy()})
 
     for DSO in dict_values["energyProviders"]:
         sector = dict_values["energyProviders"][DSO]["energyVector"]
         for DSO_source in dict_values["energyProviders"][DSO]["connected_consumption_sources"]:
-            renewable_origin[sector] += dict_values["energyProduction"][DSO_source]["total_flow"]["value"] \
+            renewable_generation[sector] += dict_values["energyProduction"][DSO_source]["total_flow"]["value"] \
                                             * dict_values["energyProviders"][DSO]["renewable_share"]["value"]
-            non_renewable_origin[sector] += dict_values["energyProduction"][DSO_source]["total_flow"]["value"] \
-                                            * (1-dict_values["energyProviders"][DSO]["renewable_share"]["value"])
 
-    dict_values["kpi"]["scalars"].update({"Total renewable energy use": renewable_origin,
-                                          "Total non-renewable energy use": non_renewable_origin})
+    dict_values["kpi"]["scalars"].update({"Total renewable energy use": renewable_generation})
     return
 
-
-def renewable_share_sector_specific(dict_values):
-    """
-    Determination of renewable share of one sector
-    :param dict_values: dict with all project information and results, after applying total_renewable_and_non_renewable_energy_origin
-    :param sector: Sector for which renewable share is being calculated
-    :return: updated dict_values with renewable share of a specific sector
-    """
-    dict_renewable_share = {}
-    for sector in dict_values["project_data"]["sectors"]:
-        total_res = dict_values["kpi"]["scalars"]["Total renewable energy use"][sector]
-        total_non_res = dict_values["kpi"]["scalars"]["Total non-renewable energy use"][sector]
-        renewable_share = total_res/(total_non_res+total_res)
-        dict_renewable_share.update({sector: renewable_share})
-
-    dict_values["kpi"]["scalars"].update({"Sector-specific renewable share": dict_renewable_share})
+def total_non_renewable_generation():
     return
--- tests/test_E3_indicator_calculation.py
+++ tests/test_E3_indicator_calculation.py
@@ -32,8 +32,6 @@ dict_renewable_energy_use["energyProduction"]["DSO_consumption_period_1"]["total
 dict_renewable_energy_use["energyProduction"]["DSO_consumption_period_2"]["total_flow"]["value"] = flow_small
 dict_renewable_energy_use["energyProduction"]["pv_plant_01"]["total_flow"]["value"] = flow_medium
 dict_renewable_energy_use["energyProviders"]["DSO"]["renewable_share"]["value"] = renewable_share_dso
-exp_res = flow_medium + (flow_small * 2 * renewable_share_dso)
-exp_non_res = (flow_small * 2 * (1 - renewable_share_dso))
 
 class TestGeneralEvaluation:
     def test_totalling_scalars_values(self):
@@ -46,16 +44,18 @@ class TestGeneralEvaluation:
     def test_total_demand_of_each_sector(self):
         assert 0 == 0
 
-    def test_total_renewable_and_non_renewable_origin_of_each_sector(self):
-        E3.total_renewable_and_non_renewable_energy_origin(dict_renewable_energy_use)
+    def test_total_renewable_generation_of_each_sector(self):
+        E3.total_renewable_energy(dict_renewable_energy_use)
+
+        print(dict_renewable_energy_use)
         assert "Total internal renewable generation" in dict_renewable_energy_use["kpi"]["scalars"]
         assert "Total renewable energy use" in dict_renewable_energy_use["kpi"]["scalars"]
-        assert "Total internal non-renewable generation" in dict_renewable_energy_use["kpi"]["scalars"]
-        assert "Total non-renewable energy use" in dict_renewable_energy_use["kpi"]["scalars"]
         assert dict_renewable_energy_use["kpi"]["scalars"]["Total internal renewable generation"]["Electricity"] == flow_medium
-        assert dict_renewable_energy_use["kpi"]["scalars"]["Total internal non-renewable generation"]["Electricity"] == 0
-        assert dict_renewable_energy_use["kpi"]["scalars"]["Total renewable energy use"]["Electricity"] == exp_res
-        assert dict_renewable_energy_use["kpi"]["scalars"]["Total non-renewable energy use"]["Electricity"] == exp_non_res
+        exp = flow_medium + (flow_small * 2 * renewable_share_dso)
+        assert dict_renewable_energy_use["kpi"]["scalars"]["Total renewable energy use"]["Electricity"] == exp
+
+    def test_total_non_renewable_generation_of_each_sector(self):
+        assert 0 == 0 #"Total non-renewable generation" in dict_generation["kpi"]["scalars"]
 
     def test_intersectoral_energy_flows_unilateral(self):
         assert 0 == 0
@@ -65,14 +65,8 @@ class TestGeneralEvaluation:
 
 
 class TestTechnicalParameters:
-    def test_renewable_share_one_sector_is_0(self):
-        assert 0 == 0
-
     def test_renewable_share_one_sector_below_1(self):
-        E3.total_renewable_and_non_renewable_energy_origin(dict_renewable_energy_use)
-        E3.renewable_share_sector_specific(dict_renewable_energy_use)
-        exp = exp_res / (exp_non_res + exp_res)
-        assert dict_renewable_energy_use["kpi"]["scalars"]["Sector-specific renewable share"]["Electricity"] == exp
+        assert 0 == 0
 
     def test_renewable_share_one_sector_is_1(self):
         assert 0 == 0
