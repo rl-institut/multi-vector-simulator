2c7b53b01e06dc8b1e1e265ca14e0a891d2419b0
smartie2076 <martha.hoffmann@rl-institut.de>
2020-04-23 11:41:55 +0200

    Finalizing functions of renewable share

--- src/E3_indicator_calculation.py
+++ src/E3_indicator_calculation.py
@@ -15,11 +15,6 @@ import pandas as pd
 from src.constants import DEFAULT_WEIGHTS_ENERGY_CARRIERS
 from src.constants import KPI_DICT, KPI_SCALARS_DICT, KPI_UNCOUPLED_DICT, KPI_COST_MATRIX
 
-class MissingDefaultParameterError(ValueError):
-    """Exception raised for missing parameters in constants.py."""
-
-    pass
-
 def all_totals(dict_values):
     """
     Calculate sum of all cost parameters
@@ -78,48 +73,66 @@ def total_renewable_and_non_renewable_energy_origin(dict_values):
 
     dict_values[KPI_DICT][KPI_UNCOUPLED_DICT].update({"Total renewable energy use": renewable_origin,
                                           "Total non-renewable energy use": non_renewable_origin})
+
+    for sector_specific_kpi in ["Total internal renewable generation", "Total internal non-renewable generation", "Total renewable energy use", "Total non-renewable energy use"]:
+        weighting_for_sector_coupled_kpi(dict_values, sector_specific_kpi)
     return
 
 
-def renewable_share_sector_specific(dict_values):
+def renewable_share(dict_values):
     """
     Determination of renewable share of one sector
     :param dict_values: dict with all project information and results, after applying total_renewable_and_non_renewable_energy_origin
     :param sector: Sector for which renewable share is being calculated
     :return: updated dict_values with renewable share of a specific sector
     """
+    kpi_name = "Renewable share"
     dict_renewable_share = {}
     for sector in dict_values["project_data"]["sectors"]:
         total_res = dict_values[KPI_DICT][KPI_UNCOUPLED_DICT]["Total renewable energy use"][sector]
         total_non_res = dict_values[KPI_DICT][KPI_UNCOUPLED_DICT]["Total non-renewable energy use"][sector]
-        renewable_share = total_res/(total_non_res+total_res)
-        dict_renewable_share.update({sector: renewable_share})
+        dict_renewable_share.update({sector: renewable_share_equation(total_res, total_non_res)})
 
-    dict_values[KPI_DICT][KPI_UNCOUPLED_DICT].update({"Renewable share": dict_renewable_share})
+    dict_values[KPI_DICT][KPI_UNCOUPLED_DICT].update({kpi_name: dict_renewable_share})
+
+    total_res = dict_values[KPI_DICT][KPI_SCALARS_DICT]["Total renewable energy use"]
+    total_non_res = dict_values[KPI_DICT][KPI_SCALARS_DICT]["Total non-renewable energy use"]
+    dict_values[KPI_DICT][KPI_SCALARS_DICT].update({kpi_name: renewable_share_equation(total_res, total_non_res)})
     return
 
-def weighting_for_sector_coupled_parameters(dict_values):
+def renewable_share_equation(total_res, total_non_res):
     """
+    Calculates the renewable share
+    :param total_res: Renewable generation of a system
+    :param total_non_res: Non-renewable generation of a system
+    :return: Renewable share
 
-    :param dict_values:  dict with all project information and results,
+    renewable share = 1 - all energy in the energy system is of renewable origin
+    renewable share < 1 - part of the energy in the system is of renewable origin
+    renewable share = 0 - no energy is of renewable orgigin
+
+    As for now this is relative to generation, but not consumption of energy, the renewable share can not be larger 1. If in future however the renewable share is calculated relative to the energy consumption, a renewable share larger 1 is possible in case of overly high renewable gerneation within the system that is later fed into the DSO grid.
+    """
+    renewable_share = total_res / (total_non_res + total_res)
+    return renewable_share
+
+def weighting_for_sector_coupled_kpi(dict_values, kpi_name):
+    """
+    Calculates the weighted kpi for a specific kpi_name
+    :param dict_values:  dict with all project information and results, including KPI_UNCOUPLED_DICT with the specifc kpi_name in question
+    :param kpi_name: str with the kpi which should be weighted
     :return: Specific KPI that describes sector-coupled system
     """
     energy_equivalent = 0
 
     for sector in dict_values["project_data"]["sectors"]:
-        if sector not in DEFAULT_WEIGHTS_ENERGY_CARRIERS:
-            raise MissingDefaultParameterError(
+        if sector in DEFAULT_WEIGHTS_ENERGY_CARRIERS:
+            energy_equivalent += dict_values[KPI_DICT][KPI_UNCOUPLED_DICT][kpi_name][sector] \
+                                 * DEFAULT_WEIGHTS_ENERGY_CARRIERS[sector]["value"]
+        else:
+            raise ValueError(
                 f"The electricity equivalent value of energy carrier {sector} is not defined. "
                 f"Please add this information to the variable DEFAULT_WEIGHTS_ENERGY_CARRIERS in constants.py.")
 
-    for sector_kpi in dict_values[KPI_DICT][KPI_UNCOUPLED_DICT]:
-        for sector in dict_values[KPI_DICT][KPI_UNCOUPLED_DICT][sector_kpi]:
-            energy_equivalent += dict_values[KPI_DICT][KPI_UNCOUPLED_DICT][sector_kpi][sector] \
-                                 * DEFAULT_WEIGHTS_ENERGY_CARRIERS[sector]["value"]
-        dict_values[KPI_DICT][KPI_SCALARS_DICT].update({sector_kpi: energy_equivalent})
-
-    #for kpi in dict_values[KPI_DICT][KPI_SCALARS_DICT]:
-    #    if dict_values[KPI_DICT][KPI_SCALARS_DICT][kpi] is dict:
-    #        "Sector-coupled KPI" =
-    #        ({"Sector-specific renewable share": dict_renewable_share})
+        dict_values[KPI_DICT][KPI_SCALARS_DICT].update({kpi_name: energy_equivalent})
     return
--- tests/test_E3_indicator_calculation.py
+++ tests/test_E3_indicator_calculation.py
@@ -5,9 +5,11 @@ import pytest
 
 import src.E3_indicator_calculation as E3
 from src.constants import KPI_DICT, KPI_SCALARS_DICT, KPI_UNCOUPLED_DICT, KPI_COST_MATRIX
+from src.constants import DEFAULT_WEIGHTS_ENERGY_CARRIERS
 
 numbers = [10, 15, 20, 25]
 
+# for test_totalling_scalars_values
 dict_scalars = {
     KPI_DICT: {
         KPI_COST_MATRIX: pd.DataFrame(
@@ -20,22 +22,56 @@ dict_scalars = {
         KPI_SCALARS_DICT: {},
     }
 }
-
 scalars_expected = {"cost": numbers[1] + numbers[3], "annuity": numbers[0] + numbers[2]}
 
+# for this test_total_renewable_and_non_renewable_origin_of_each_sector test
 with open(os.path.join("tests", "test_data", "test_json_for_E3_res.json")) as json_file:
     dict_renewable_energy_use = json.load(json_file)
 
 flow_small = 50
 flow_medium = 100
 renewable_share_dso = 0.1
-
 dict_renewable_energy_use["energyProduction"]["DSO_consumption_period_1"]["total_flow"]["value"] = flow_small
 dict_renewable_energy_use["energyProduction"]["DSO_consumption_period_2"]["total_flow"]["value"] = flow_small
 dict_renewable_energy_use["energyProduction"]["pv_plant_01"]["total_flow"]["value"] = flow_medium
 dict_renewable_energy_use["energyProviders"]["DSO"]["renewable_share"]["value"] = renewable_share_dso
 exp_res = flow_medium + (flow_small * 2 * renewable_share_dso)
 exp_non_res = (flow_small * 2 * (1 - renewable_share_dso))
+
+# for weighting_for_sector_coupled_kpi
+a_kpi_name = "renewable share"
+dict_weighting_unknown_sector = {
+    "project_data": {
+        "sectors": {
+            "Heat": "He"
+        }},
+    KPI_DICT: {
+        KPI_UNCOUPLED_DICT: {a_kpi_name: {"Heat": numbers[0]}},
+    KPI_SCALARS_DICT: {}
+    }
+}
+
+dict_weighting_one_sector = {
+    "project_data": {
+        "sectors": {
+            "Electricity": "E"}},
+    KPI_DICT: {
+        KPI_UNCOUPLED_DICT: {a_kpi_name: {"Electricity": numbers[1]}},
+    KPI_SCALARS_DICT: {}
+    }
+}
+
+dict_weighting_two_sectors = {
+    "project_data": {
+        "sectors": {
+            "Electricity": "E",
+            "H2": "H"}},
+    KPI_DICT: {
+        KPI_UNCOUPLED_DICT: {a_kpi_name: {"Electricity": numbers[1],
+                                          "H2": numbers[2]}},
+    KPI_SCALARS_DICT: {}
+    }
+}
 
 class TestGeneralEvaluation:
     def test_totalling_scalars_values(self):
@@ -54,6 +90,10 @@ class TestGeneralEvaluation:
         assert "Total renewable energy use" in dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]
         assert "Total internal non-renewable generation" in dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]
         assert "Total non-renewable energy use" in dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]
+        assert "Total internal renewable generation" in dict_renewable_energy_use[KPI_DICT][KPI_SCALARS_DICT]
+        assert "Total renewable energy use" in dict_renewable_energy_use[KPI_DICT][KPI_SCALARS_DICT]
+        assert "Total internal non-renewable generation" in dict_renewable_energy_use[KPI_DICT][KPI_SCALARS_DICT]
+        assert "Total non-renewable energy use" in dict_renewable_energy_use[KPI_DICT][KPI_SCALARS_DICT]
         assert dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]["Total internal renewable generation"]["Electricity"] == flow_medium
         assert dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]["Total internal non-renewable generation"]["Electricity"] == 0
         assert dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]["Total renewable energy use"]["Electricity"] == exp_res
@@ -65,43 +105,48 @@ class TestGeneralEvaluation:
     def test_intersectoral_energy_flows_bilateral(self):
         assert 0 == 0
 
-
 class TestTechnicalParameters:
-    #def test_weighting_for_sector_coupled_parameters_unknown_parameters(self):
-    #    with pytest.raises(TypeError):
-    #        E3.weighting_for_sector_coupled_parameters()
+    def test_weighting_for_sector_coupled_kpi_unknown_sector(self):
+        with pytest.raises(ValueError):
+            E3.weighting_for_sector_coupled_kpi(dict_weighting_unknown_sector, a_kpi_name)
 
-    #def test_weighting_for_sector_coupled_parameters_one_sector(self):
-    #    E3.weighting_for_sector_coupled_parameters()
-    #    assert 0 == 0
+    def test_weighting_for_sector_coupled_kpi_one_sector(self):
+        E3.weighting_for_sector_coupled_kpi(dict_weighting_one_sector, a_kpi_name)
+        assert a_kpi_name in dict_weighting_one_sector[KPI_DICT][KPI_SCALARS_DICT]
+        assert dict_weighting_one_sector[KPI_DICT][KPI_SCALARS_DICT][a_kpi_name] == numbers[1]
 
-    #def test_weighting_for_sector_coupled_parameters_multiple_sectors(self):
-    #    E3.weighting_for_sector_coupled_parameters()
-    #    assert 0 == 0
+    def test_weighting_for_sector_coupled_kpi_multiple_sectors(self):
+        E3.weighting_for_sector_coupled_kpi(dict_weighting_two_sectors, a_kpi_name)
+        exp = numbers[1] + numbers[2] * DEFAULT_WEIGHTS_ENERGY_CARRIERS["H2"]["value"]
+        assert a_kpi_name in dict_weighting_two_sectors[KPI_DICT][KPI_SCALARS_DICT]
+        assert dict_weighting_two_sectors[KPI_DICT][KPI_SCALARS_DICT][a_kpi_name] == exp
 
-    def test_renewable_share_one_sector_is_0(self):
-        assert 0 == 0
-
-    def test_renewable_share_one_sector_below_1(self):
+    def test_renewable_share_one_sector(self):
         E3.total_renewable_and_non_renewable_energy_origin(dict_renewable_energy_use)
-        E3.renewable_share_sector_specific(dict_renewable_energy_use)
+        E3.renewable_share(dict_renewable_energy_use)
         exp = exp_res / (exp_non_res + exp_res)
+        assert "Renewable share" in dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]
+        assert "Electricity" in dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]["Renewable share"]
         assert dict_renewable_energy_use[KPI_DICT][KPI_UNCOUPLED_DICT]["Renewable share"]["Electricity"] == exp
+        assert dict_renewable_energy_use[KPI_DICT][KPI_SCALARS_DICT]["Renewable share"] == exp
 
-    def test_renewable_share_one_sector_is_1(self):
-        assert 0 == 0
+    def test_renewable_share_equation_is_1(self):
+        tot_res = 100
+        tot_non_res = 0
+        renewable_share = E3.renewable_share_equation(tot_res, tot_non_res)
+        assert renewable_share == tot_res/(tot_res+tot_non_res)
 
-    def test_renewable_share_one_sector_above_1(self):
-        assert 0 == 0
+    def test_renewable_share_equation_is_0(self):
+        tot_res = 0
+        tot_non_res = 100
+        renewable_share = E3.renewable_share_equation(tot_res, tot_non_res)
+        assert renewable_share == tot_res/(tot_res+tot_non_res)
 
-    def test_renewable_share_two_sectors_below_1(self):
-        assert 0 == 0
-
-    def test_renewable_share_two_sectors_is_1(self):
-        assert 0 == 0
-
-    def test_renewable_share_two_sectors_above_1(self):
-        assert 0 == 0
+    def test_renewable_share_equation_below_1(self):
+        tot_res = 20
+        tot_non_res = 100
+        renewable_share = E3.renewable_share_equation(tot_res, tot_non_res)
+        assert renewable_share == tot_res/(tot_res+tot_non_res)
 
     def test_degree_of_autonomy_below_1(self):
         assert 0 == 0
