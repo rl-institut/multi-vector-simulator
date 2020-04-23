bfbfc363cfb3069cbba10743f993c8cf56b652b8
SabineH <sabine.haas@rl-institut.de>
2020-04-23 10:24:30 +0200

    Fix constants

--- tests/constants.py
+++ tests/constants.py
@@ -8,9 +8,13 @@ TEST_REPO_PATH = os.path.dirname(__file__)
 CSV_PATH = os.path.join(TEST_REPO_PATH, INPUT_FOLDER, CSV_ELEMENTS)
 JSON_PATH = os.path.join(TEST_REPO_PATH, INPUT_FOLDER, JSON_FNAME)
 
+# input directory for tests
+TEST_INPUT_DIRECTORY = "inputs"
 # input directory for file you need for specific tests
-SPECIFIC_TESTS_INPUT = "input_for_specific_tests"
+SPECIFIC_TEST_INPUT_DIRECTORY = "inputs_for_specific_tests"
+
 
 # json file for specific D1 tests
-D1_JSON = os.path.join(TEST_REPO_PATH, SPECIFIC_TESTS_INPUT,
+D1_JSON = os.path.join(TEST_REPO_PATH, TEST_INPUT_DIRECTORY,
+                       SPECIFIC_TEST_INPUT_DIRECTORY,
                        "mvs_config_for_D1_tests.json")
