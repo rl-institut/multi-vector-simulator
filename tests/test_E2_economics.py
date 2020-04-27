ac0f83ddb65dda4ff68b1820900a501d5a1c83b1
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-04-27 12:01:25 +0200

    Define names of test functions

---
+++ tests/test_E2_economics.py
@@ -0,0 +1,41 @@
+import src.E2_economics
+
+def test_total_costs_with_lifetime():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_total_costs_with_upfront_costs():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_om_costs_with_lifetime():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_om_costs_with_energy_costs():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_total_costs_and_om_costs():
+    """
+
+    Returns
+    -------
+
+    """
