2b515825a3177a1d20c352477ed233253ff9ba08
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-04-27 16:01:41 +0200

    Define names of test functions

---
+++ tests/test_E2_economics.py
@@ -0,0 +1,30 @@
+import src.E2_economics as E2
+
+def test_get_costs():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_add_costs_and_total():
+    """
+
+    Returns
+    -------
+
+    """
+
+def test_all_list_in_dict():
+    """
+
+    Parameters
+    ----------
+    dict_asset
+    list
+
+    Returns
+    -------
+
+    """
