864f4bdf042efce2026a02c8cd72ba098be17166
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-04-27 16:59:47 +0200

    Add test to check sum of all in/out to bus is 0 in case AB

--- tests/test_benchmark_simple_scenarios.py
+++ tests/test_benchmark_simple_scenarios.py
@@ -10,6 +10,7 @@ import argparse
 import shutil
 import mock
 import pytest
+import pandas as pd
 
 from .constants import TEST_REPO_PATH, JSON_EXT, CSV_EXT
 from mvs_eland_tool.mvs_eland_tool import main
@@ -35,10 +36,21 @@ class TestACElectricityBus:
         use_case = "AB"
         main(
             path_input_folder=os.path.join(TEST_INPUT_PATH, use_case),
-            input_type=JSON_EXT,
+            input_type=CSV_EXT,
             path_output_folder=os.path.join(TEST_OUTPUT_PATH, use_case),
         )
-        assert 1 == 1
+
+        df_busses_flow = pd.read_excel(
+            os.path.join(TEST_OUTPUT_PATH, use_case, "timeseries_all_busses.xlsx"),
+            sheet_name="Electricity bus",
+        )
+        # make the time the index
+        df_busses_flow = df_busses_flow.set_index("Unnamed: 0")
+        # compute the sum of the in and out of the electricity bus
+        df_busses_flow["net_sum"] = df_busses_flow.sum(axis=1)
+
+        # make sure the sum of the bus flow is always zero (there are rounding errors)
+        assert df_busses_flow.net_sum.map(lambda x: 0 if x < 1e-4 else 1).sum() == 0
 
     def teardown_method(self):
         if os.path.exists(TEST_OUTPUT_PATH):
