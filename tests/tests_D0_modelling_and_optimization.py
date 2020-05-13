4c979858ac14c3aa34193cc4cc2523f277914dd6
smartie2076 <martha.hoffmann@rl-institut.de>
2020-05-13 16:26:56 +0200

    assertion 1 == 0

--- tests/tests_D0_modelling_and_optimization.py
+++ tests/tests_D0_modelling_and_optimization.py
@@ -1,49 +1,49 @@
-def test_if_oemof_simulation_runs_through:
-    assert False
+def test_if_oemof_simulation_runs_through(self):
+    assert 1 == 0
     
-def test_if_error_raise_if_type_oemof_not_defined:
-    assert False
+def test_if_error_raise_if_type_oemof_not_defined(self):
+    assert 1 == 0
     
-def test_if_model_building_time_measured_and_stored:
-    assert False
+def test_if_model_building_time_measured_and_stored(self):
+    assert 1 == 0
     
-def test_if_model_simulation_time_measured_and_stored:
-    assert False
+def test_if_model_simulation_time_measured_and_stored(self):
+    assert 1 == 0
  
-def test_if_oemof_results_are_stored_to_file_if_setting_true:
-    assert False
+def test_if_oemof_results_are_stored_to_file_if_setting_true(self):
+    assert 1 == 0
     
-def test_if_lp_file_is_stored_to_file_if_setting_true:
-    assert False
+def test_if_lp_file_is_stored_to_file_if_setting_true(self):
+    assert 1 == 0
     
-def test_if_networkx_graph_function_called:
-    assert False
+def test_if_networkx_graph_function_called(self):
+    assert 1 == 0
 
-def test_if_simulation_parameters_added_to_dict:
-    assert False
+def test_if_simulation_parameters_added_to_dict(self):
+    assert 1 == 0
     
-def test_if_result_data_for_E0_in_correct_format:
-    assert False
+def test_if_result_data_for_E0_in_correct_format(self):
+    assert 1 == 0
 
-def test_if_constraint_settings_add_constraints:
-    assert False
+def test_if_constraint_settings_add_constraints(self):
+    assert 1 == 0
     
-def test_if_all_assets_added_to_dict_oemof_components:
-    assert False
+def test_if_all_assets_added_to_dict_oemof_components(self):
+    assert 1 == 0
 
-def test_if_energy_conversion_assets_added:
-    assert False
+def test_if_energy_conversion_assets_added(self):
+    assert 1 == 0
  
-def test_if_energy_production_assets_added:
-    assert False
+def test_if_energy_production_assets_added(self):
+    assert 1 == 0
     
-def test_if_energy_provider_assets_added:
-    assert False    
+def test_if_energy_provider_assets_added(self):
+    assert 1 == 0    
 
-def test_if_energy_system_model_generated_by_oemof:
-    assert False
-def test_if_energy_storage_assets_added:
-    assert False
+def test_if_energy_system_model_generated_by_oemof(self):
+    assert 1 == 0
+def test_if_energy_storage_assets_added(self):
+    assert 1 == 0
     
-def test_if_energy_busses_added:
-    assert False
+def test_if_energy_busses_added(self):
+    assert 1 == 0
