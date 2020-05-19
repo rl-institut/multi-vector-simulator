d19d933dd05fb33b370e3e2e5728d0b1addb6abf
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-05-19 15:50:32 +0200

    Read env variable to skip tests
    By setting it to 'dev' one can select which tests will be ran
    on the dev branch. Ibid when setting it to 'master'.

--- tests/constants.py
+++ tests/constants.py
@@ -3,6 +3,9 @@ import os
 # import constants from src
 from src.constants import (
     INPUT_FOLDER,
+    OUTPUT_FOLDER,
+    DEFAULT_INPUT_PATH,
+    DEFAULT_OUTPUT_PATH,
     JSON_FNAME,
     JSON_EXT,
     CSV_ELEMENTS,
@@ -15,6 +18,11 @@ from src.constants import (
     PDF_REPORT,
     DICT_PLOTS,
 )
+
+TESTS_ON_MASTER = "master"
+TESTS_ON_DEV = "dev"
+
+EXECUTE_TESTS_ON = os.environ.get("EXECUTE_TESTS_ON", "skip")
 
 TEST_REPO_PATH = os.path.dirname(__file__)
 
