a85ac5ddb4b371b43f1429efbd8f791e210ccea0
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-05-20 10:04:31 +0200

    Add user info

--- mvs_report.py
+++ mvs_report.py
@@ -17,5 +17,13 @@ if __name__ == "__main__":
         # load the results of a simulation
         dict_values = load_json(fname)
         test_app = create_app(dict_values)
+        banner = "*" * 40
+        print(banner + "\nPress ctrl+c to stop the report server\n" + banner)
         # run the dash server for 600s before shutting it down
         open_in_browser(test_app, timeout=600)
+        print(
+            banner
+            + "\nThe report server has timed out.\nTo start it again run `python "
+            "mvs_report.py`.\nTo let it run for a longer time, change timeout setting in "
+            "the mvs_report.py file\n" + banner
+        )
