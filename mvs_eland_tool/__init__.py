4c60b5785302562eca79c455fb32a97f0f36d00a
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-05-24 18:52:33 +0200

    Move version number and date at package level

--- mvs_eland_tool/__init__.py
+++ mvs_eland_tool/__init__.py
@@ -1,1 +1,5 @@
+version = "0.2.0"  # update_me Versioning scheme: Major release.Minor release.Patches
+version_date = "2020-03-13"  # update_me Update date
+
+
 from mvs_eland_tool.local_deploy import main as main
