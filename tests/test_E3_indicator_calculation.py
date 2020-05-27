c94cbf8e75271b681887b398ffb706a0a20f2dcf
smartie2076 <martha.hoffmann@rl-institut.de>
2020-05-27 11:07:33 +0200

    Formatting with blacks

--- tests/test_E3_indicator_calculation.py
+++ tests/test_E3_indicator_calculation.py
@@ -31,7 +31,10 @@ scalars_expected = {"cost": numbers[1] + numbers[3], "annuity": numbers[0] + num
 
 # for this test_total_renewable_and_non_renewable_origin_of_each_sector test
 from src.B0_data_input_json import load_json
-dict_renewable_energy_use = load_json(os.path.join("tests", "test_data", "test_json_for_E3.json"))
+
+dict_renewable_energy_use = load_json(
+    os.path.join("tests", "test_data", "test_json_for_E3.json")
+)
 
 flow_small = 50
 flow_medium = 100
