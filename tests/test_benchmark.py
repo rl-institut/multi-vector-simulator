cf1d3b3726f2e94ed339cfe3a59274f1a1ba8430
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-05-29 17:51:21 +0200

    Add test for server simulation
    The server simulation take only a json dict as input.
    The dict should already have the timeseries in it and not depend
    on any additional files.

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -5,19 +5,21 @@ What should differ between the different functions is the input file
 
 """
 import argparse
+import json
 import os
 import shutil
 
 import mock
 import pytest
 
-from mvs_eland_tool import main
+from mvs_eland_tool import main, run_simulation
+from src.B0_data_input_json import load_json
 from .constants import (
     EXECUTE_TESTS_ON,
     TESTS_ON_MASTER,
     TESTS_ON_DEV,
     TEST_REPO_PATH,
-    JSON_PATH,
+    JSON_FNAME,
     JSON_EXT,
     CSV_EXT,
 )
@@ -72,3 +74,20 @@ class TestLocalSimulation:
     def teardown_method(self):
         if os.path.exists(OUTPUT_PATH):
             shutil.rmtree(OUTPUT_PATH, ignore_errors=True)
+
+
+class TestServerSimulation:
+    def setup_class(self):
+        with open(os.path.join(RERUN_PATH, JSON_FNAME)) as fp:
+            self.input_json = load_json(fp)
+
+    # this ensure that the test is only ran if explicitly executed, ie not when the `pytest` command
+    # alone it called
+    @pytest.mark.skipif(
+        EXECUTE_TESTS_ON not in (TESTS_ON_MASTER),
+        reason="Benchmark test deactivated, set env variable "
+        "EXECUTE_TESTS_ON to 'master' to run this test",
+    )
+    def test_run_smoothly_json(self):
+        run_simulation(self.input_json)
+        assert 1 == 1
