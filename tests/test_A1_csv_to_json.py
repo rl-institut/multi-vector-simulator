d13023fbf2f12b6bef10d4378807106a41ca2670
smartie2076 <gitMA18rli>
2020-06-16 12:59:44 +0200

    Defining A1.check_for_newly_added_parameters to enable easy new definition of new parameters

--- tests/test_A1_csv_to_json.py
+++ tests/test_A1_csv_to_json.py
@@ -1,9 +1,16 @@
 import os
 
 import pytest
+import pandas as pd
 
 import src.A1_csv_to_json as A1
 import src.B0_data_input_json as data_input
+
+from src.constants import (
+    WARNING_TEXT,
+    REQUIRED_IN_CSV_ELEMENTS,
+)
+
 from src.constants_json_strings import (
     UNIT,
     VALUE,
@@ -57,6 +64,32 @@ CONVERSION_TYPE = {
     "param_year": {UNIT: "year", VALUE: 8},
 }
 
+
+### Test function
+
+filename_a = "a_file"
+filename_b = "b_file"
+df_no_new_parameter = pd.DataFrame(["a", "b"], index = ["unit", "value"])
+
+parameters = ["unit", "value"]
+
+list_of_new_parameter = {"max": {
+    WARNING_TEXT: "a test warning",
+    REQUIRED_IN_CSV_ELEMENTS: [filename_a]}}
+
+def test_if_check_for_newly_added_parameter_adds_no_parameter_when_not_necessary():
+    parameters_updated = A1.check_for_newly_added_parameters(filename_b, df_no_new_parameter, parameters, list_of_new_parameter)
+    assert parameters == parameters_updated
+
+def test_if_check_for_newly_added_parameter_raises_warning_if_parameter_doesnt_exist():
+    with pytest.raises(A1.MissingParameterError):
+        parameters_updated = A1.check_for_newly_added_parameters(filename_a, df_no_new_parameter, parameters, list_of_new_parameter)
+
+df_with_new_parameter = pd.DataFrame(["a", "b", 20], index = ["unit", "value", "max"])
+
+def test_if_check_for_newly_added_parameter_adds_to_parameter_list_when_new_parameter_exists():
+    parameters_updated = A1.check_for_newly_added_parameters(filename_a, df_with_new_parameter, parameters, list_of_new_parameter)
+    assert ["unit", "value", "max"] == parameters_updated
 
 def test_create_input_json_creation_of_json_file():
     A1.create_input_json(input_directory=CSV_PATH)
