156215245dbecd6592bc32dd83b63e2651bdb8b9
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-06-26 09:32:44 +0200

    Add test to ensure input folders have required parameters
    The test concentrates on the csv files inputs

---
+++ tests/test_input_folder_parameters.py
@@ -0,0 +1,13 @@
+import os
+import pytest
+
+from src.utils import find_input_folders, compare_input_parameters_with_reference
+from src.constants import REPO_PATH
+from .constants import TEST_REPO_PATH, JSON_EXT, CSV_EXT, MISSING_PARAMETERS_KEY
+
+TEST_INPUT_FOLDERS = find_input_folders(REPO_PATH)
+
+@pytest.mark.parametrize("input_folder", TEST_INPUT_FOLDERS)
+def test_input_folder_csv_files_have_required_parameters(input_folder):
+    comparison = compare_input_parameters_with_reference(input_folder, ext=CSV_EXT)
+    assert MISSING_PARAMETERS_KEY not in comparison
