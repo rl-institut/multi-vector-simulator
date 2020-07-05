ad7a7f49bc314260f04242313f1f73770e517202
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-07-06 00:11:31 +0200

    Move version num and date in a separate file

--- mvs_eland_tool/__init__.py
+++ mvs_eland_tool/__init__.py
@@ -1,6 +1,2 @@
-version = "0.3.0"  # update_me Versioning scheme: Major release.Minor release.Patches
-version_date = "2020-06-08"  # update_me Update date
-
-
 from mvs_eland_tool.local_deploy import main as main
 from mvs_eland_tool.server_deploy import main as run_simulation
