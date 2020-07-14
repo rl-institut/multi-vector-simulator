fafb43756d4205acebde6364360a489e6461d9f3
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-07-14 13:01:33 +0200

    Add LCOE_ASSET to constants_json_strings.py

--- src/constants_json_strings.py
+++ src/constants_json_strings.py
@@ -171,6 +171,9 @@ COST_DISPATCH = (
 COST_UPFRONT = "costs_upfront_in_year_zero"
 COST_INVESTMENT = "costs_investment_over_lifetime"
 
+# Levelized cost of electricity
+LCOE_ASSET = "levelized_cost_of_electricity_of_asset"
+
 # KPI_FLOW_MATRIX
 KPI_SCALARS = (
     ANNUITY_OM,
