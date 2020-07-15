e729af55eca24d0164ea3a8c3ea6415c12c46246
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-07-15 23:39:29 +0200

    Attempt to write LCOE test function

--- tests/test_E2_economics.py
+++ tests/test_E2_economics.py
@@ -1,4 +1,17 @@
+import logging
+import os
+import pickle
+import shutil
+
+import mock
+
+import src.A0_initialization as initializing
+import src.B0_data_input_json as data_input
+import src.C0_data_processing as data_processing
+import src.D0_modelling_and_optimization as modelling
+import src.E0_evaluation as evaluation
 import src.E2_economics as E2
+
 from src.constants_json_strings import (
     UNIT,
     CURR,
@@ -22,6 +35,14 @@ from src.constants_json_strings import (
     COST_OM_FIX,
 )
 
+from .constants import (
+    TEST_REPO_PATH,
+    INPUT_FOLDER,
+    PATH_INPUT_FILE,
+    PATH_INPUT_FOLDER,
+    PATH_OUTPUT_FOLDER,
+)
+
 dict_asset = {
     LABEL: "DSO_feedin_sink",
     DISPATCH_PRICE: {VALUE: -0.4, UNIT: "currency/kWh"},
@@ -38,6 +59,14 @@ dict_asset = {
 dict_economic = {
     CRF: {VALUE: 0.07264891149004721, UNIT: "?"},
 }
+
+PARSER = initializing.create_parser()
+TEST_INPUT_PATH = os.path.join(TEST_REPO_PATH, INPUT_FOLDER)
+TEST_OUTPUT_PATH = os.path.join(TEST_REPO_PATH, "MVS_outputs")
+
+DICT_BEFORE = os.path.join(TEST_REPO_PATH, "dict_values_before_E0.pickle")
+
+DICT_AFTER = os.path.join(TEST_REPO_PATH, "dict_values_after_E0.pickle")
 
 
 def test_all_cost_info_parameters_added_to_dict_asset():
@@ -77,3 +106,45 @@ def test_all_list_in_dict_fails_due_to_not_included_keys():
     list_false = ["flow", OPTIMIZED_ADD_CAP]
     boolean = E2.all_list_in_dict(dict_asset, list_false)
     assert boolean is False
+
+@mock.patch(
+    "argparse.ArgumentParser.parse_args",
+    return_value=PARSER.parse_args(["-i", TEST_INPUT_PATH, "-o", TEST_OUTPUT_PATH]),
+)
+def setup_module(m_args):
+    """Run the simulation up to module E0 and save dict_values before and after evaluation"""
+    if os.path.exists(TEST_OUTPUT_PATH):
+        shutil.rmtree(TEST_OUTPUT_PATH, ignore_errors=True)
+    user_input = initializing.process_user_arguments()
+
+    logging.debug("Accessing script: B0_data_input_json")
+    dict_values = data_input.load_json(
+        user_input[PATH_INPUT_FILE],
+        path_input_folder=user_input[PATH_INPUT_FOLDER],
+        path_output_folder=user_input[PATH_OUTPUT_FOLDER],
+        move_copy=False,
+    )
+    logging.debug("Accessing script: C0_data_processing")
+    data_processing.all(dict_values)
+
+    logging.debug("Accessing script: D0_modelling_and_optimization")
+    results_meta, results_main = modelling.run_oemof(dict_values)
+
+    with open(DICT_BEFORE, "wb") as handle:
+        pickle.dump(dict_values, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    logging.debug("Accessing script: E0_evaluation")
+    evaluation.evaluate_dict(dict_values, results_main, results_meta)
+
+    with open(DICT_AFTER, "wb") as handle:
+        pickle.dump(dict_values, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def test_lcoe_parameter_added_to_dict_asset():
+    with open(DICT_BEFORE, "rb") as handle:
+        dict_values_before = pickle.load(handle)
+
+    with open(DICT_AFTER, "rb") as handle:
+        dict_values_after = pickle.load(handle)
+
+    assert "LCOE" not in dict_values_before
+    assert "LCOE" in dict_values_after
