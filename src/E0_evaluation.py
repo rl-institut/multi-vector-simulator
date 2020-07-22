de7e35481eabe26416c58a12a3f007985fd5d1ba
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-07-22 17:54:16 +0200

    Use equality instead of isinstance

--- src/E0_evaluation.py
+++ src/E0_evaluation.py
@@ -174,7 +174,7 @@ def store_result_matrix(dict_kpi, dict_asset):
             if key in dict_asset:
                 if isinstance(dict_asset[key], str):
                     asset_result_dict.update({key: dict_asset[key]})
-                elif isinstance(dict_asset[key][VALUE], None):
+                elif dict_asset[key][VALUE] == None:
                     asset_result_dict.update({key: None})
                 else:
                     asset_result_dict.update(
