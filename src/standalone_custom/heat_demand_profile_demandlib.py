20914252c46d0b8f6410167fdbf7736f3033f1ca
SabineH <sabine.haas@rl-institut.de>
2020-07-23 15:41:07 +0200

    apply black formatting

--- src/standalone_custom/heat_demand_profile_demandlib.py
+++ src/standalone_custom/heat_demand_profile_demandlib.py
@@ -64,7 +64,9 @@ annual_demand_gas = gas_consumption * efficiency_gas_boiler  # in kWh
 
 # add annual collectors heat to annual demand
 filename_coll = os.path.join(
-    path_to_data_folder, "2020-06-17_solar_thermal_collector", "solar_thermal_collectors_heat.csv"
+    path_to_data_folder,
+    "2020-06-17_solar_thermal_collector",
+    "solar_thermal_collectors_heat.csv",
 )
 collectors_heat = pd.read_csv(filename_coll, index_col=0, header=0).sum().sum()
 annual_demand = annual_demand_gas + collectors_heat  # in kWh
@@ -193,21 +195,28 @@ def get_workalendar_class(country):
 
 if __name__ == "__main__":
     plots = True
-    weather_data_name = "uvtgv"  # "uvtgv" is processed monitored data, "era5" is ERA5 data
+    weather_data_name = (
+        "uvtgv"  # "uvtgv" is processed monitored data, "era5" is ERA5 data
+    )
 
     # load weather
     if weather_data_name == "era5":
-        weather = pd.read_csv(f"era5_weather_UVTgV_{year}.csv", parse_dates=True).set_index(
-            "time"
-        )
+        weather = pd.read_csv(
+            f"era5_weather_UVTgV_{year}.csv", parse_dates=True
+        ).set_index("time")
         weather.index = pd.to_datetime(weather.index, utc=True).tz_convert(time_zone)
         weather.reset_index("time", inplace=True)
     elif weather_data_name == "uvtgv":
         filename_weather = "enter_filename_including_path"
+        filename_weather = os.path.join(
+            path_to_data_folder, "2020-07-23_uvtgv_weather.csv"
+        )
         cols = {"Amb Temp": "temp_air"}
         weather = pd.read_csv(filename_weather).rename(columns=cols)
     else:
-        raise ValueError(f"weather_data_name must be 'era5' or 'uvtgv' but is {weather_data_name}")
+        raise ValueError(
+            f"weather_data_name must be 'era5' or 'uvtgv' but is {weather_data_name}"
+        )
 
     demand = calculate_heat_demand_time_series(
         year=year,
