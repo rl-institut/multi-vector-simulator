71d950f22641a3ca5ebfdf4ddbfcd83b0527e6ef
smartie2076 <martha.hoffmann@rl-institut.de>
2020-08-03 13:06:52 +0200

    Reformatting with black

--- setup.py
+++ setup.py
@@ -133,8 +133,10 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "oemof.solph==0.4.1", "oemof.network==0.4.0rc0", "oemof.tools==0.4.0"
-    ],  # Optional  
+        "oemof.solph==0.4.1",
+        "oemof.network==0.4.0rc0",
+        "oemof.tools==0.4.0",
+    ],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
