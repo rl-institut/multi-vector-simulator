fc6055d1ea321bf6edc5294711b1a27d4095fe2e
smartie2076 <martha.hoffmann@rl-institut.de>
2020-08-03 12:53:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -133,8 +133,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "oemof"
-    ],  # Optional  # todo: later exchange to oemof.solph>=0.4.0, oemof.network>=0.4.0 oemof.tools>=??
+        "oemof.solph==0.4.1", "oemof.network==0.4.0rc0", "oemof.tools==0.4.0"
+    ],  # Optional  
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
