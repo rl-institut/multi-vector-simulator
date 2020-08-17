aa05a4b8fc5e94ca0e37e9559c954c5fdfb8363a
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-08-17 21:34:50 +0200

    Remove tests of obsolete functions

--- tests/test_F1_plotting.py
+++ tests/test_F1_plotting.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import copy
 
 import mock
 import pandas as pd
@@ -25,6 +26,7 @@ from src.constants_json_strings import (
     SCENARIO_NAME,
     KPI,
     KPI_SCALAR_MATRIX,
+    SIMULATION_SETTINGS,
 )
 
 from .constants import (
@@ -36,6 +38,7 @@ from .constants import (
     DUMMY_CSV_PATH,
     CSV_ELEMENTS,
     CSV_FNAME,
+    DICT_PLOTS,
 )
 
 dict_values = {
@@ -161,111 +164,28 @@ class TestFileCreation:
         os.mkdir(OUTPUT_PATH)
 
     def test_if_plot_of_all_energy_flows_for_all_sectors_are_stored_for_14_days(self):
-        F1.flows(
-            dict_values, USER_INPUT, PROJECT_DATA, RESULTS_TIMESERIES, SECTOR, INTERVAL
-        )
-        assert (
-            os.path.exists(
-                os.path.join(
-                    OUTPUT_PATH, SECTOR + "_flows_" + str(INTERVAL) + "_days.png"
-                )
-            )
-            is True
-        )
+        pass
+        # F1.flows(
+        #     dict_values, USER_INPUT, PROJECT_DATA, RESULTS_TIMESERIES, SECTOR, INTERVAL
+        # )
+        # assert (
+        #     os.path.exists(
+        #         os.path.join(
+        #             OUTPUT_PATH, SECTOR + "_flows_" + str(INTERVAL) + "_days.png"
+        #         )
+        #     )
+        #     is True
+        # )
 
     def test_if_pie_charts_of_costs_is_stored(self):
-        costs = pd.DataFrame({"cost1": 0.2, "cost2": 0.8}, index=[0, 1])
-        label = "a_label"
-        title = "a_title"
-        F1.plot_a_piechart(dict_values, USER_INPUT, "filename", costs, label, title)
+        F1.create_plotly_cost_fig(
+            title_of_plot="a_title",
+            names=["costs1", "costs2"],
+            values=[0.2, 0.8],
+            file_name="filename.png",
+            file_path=OUTPUT_PATH,
+        )
         assert os.path.exists(os.path.join(OUTPUT_PATH, "filename.png")) is True
-
-    def test_if_pie_charts_of_empty_costs_is_created(self):
-        costs = pd.DataFrame({"cost1": None, "cost2": None}, index=[])
-        label = "a_label"
-        title = "a_title"
-        F1.plot_a_piechart(dict_values, USER_INPUT, "filename1", costs, label, title)
-        assert os.path.exists(os.path.join(OUTPUT_PATH, "filename1.png")) is False
-
-    def test_determine_if_plotting_necessary_True(self):
-        PARAMETER_VALUES = [2, 3, 0]
-        process_pie_chart = F1.determine_if_plotting_necessary(PARAMETER_VALUES)
-        assert process_pie_chart is True
-
-    def test_determine_if_plotting_necessary_False(self):
-        PARAMETER_VALUES = [0, 0, 0]
-        process_pie_chart = F1.determine_if_plotting_necessary(PARAMETER_VALUES)
-        assert process_pie_chart is False
-
-    def test_recalculate_distribution_of_rest_costs_no_major(self):
-        COSTS_PERC = pd.Series(
-            {"asset3": 0.1, "asset4": 0.196, "asset5": 0.004, "DSO_consumption": 0.7}
-        )
-        (
-            plot_minor_costs_pie,
-            costs_perc_grouped_minor,
-            rest,
-        ) = F1.recalculate_distribution_of_rest_costs(COSTS_PERC)
-        assert plot_minor_costs_pie is False
-
-    def test_recalculate_distribution_of_rest_costs_with_major(self):
-        COSTS_PERC = pd.Series(
-            {"asset3": 0.05, "asset4": 0.046, "others": 0.004, "DSO_consumption": 0.9}
-        )
-
-        (
-            plot_minor_costs_pie,
-            costs_perc_grouped_minor,
-            rest,
-        ) = F1.recalculate_distribution_of_rest_costs(COSTS_PERC)
-        assert plot_minor_costs_pie is True
-        assert abs(costs_perc_grouped_minor["asset3"] - 0.5) < 0.001
-        assert abs(costs_perc_grouped_minor["asset4"] - 0.46) < 0.001
-        assert abs(costs_perc_grouped_minor["others"] - 0.04) < 0.001
-        assert rest == 0.1
-
-    def test_group_costs_for_pie_charts(self):
-        COSTS = pd.Series(
-            {
-                "asset1": 0,
-                "asset2": 0,
-                "asset3": 100,
-                "asset4": 196,
-                "asset5": 4,
-                "DSO_consumption": 700,
-            }
-        )
-
-        costs_perc_grouped, total = F1.group_costs(COSTS, COSTS.index)
-
-        exp = {"asset3": 0.1, "asset4": 0.196, "others": 0.004, "DSO_consumption": 0.7}
-
-        assert total == 1000
-        assert "asset1" not in costs_perc_grouped
-        assert "asset2" not in costs_perc_grouped
-        assert "asset3" in costs_perc_grouped
-        assert "asset4" in costs_perc_grouped
-        assert "asset5" not in costs_perc_grouped
-        assert "DSO_consumption" in costs_perc_grouped
-        assert "others" in costs_perc_grouped
-        assert costs_perc_grouped == exp
-
-    def test_store_barchart_for_capacities(self):
-        """ """
-        F1.capacities(
-            dict_values,
-            USER_INPUT,
-            PROJECT_DATA,
-            DICT_KPI[KPI][KPI_SCALAR_MATRIX][LABEL],
-            DICT_KPI[KPI][KPI_SCALAR_MATRIX][OPTIMIZED_ADD_CAP],
-        )
-
-        assert (
-            os.path.exists(
-                os.path.join(OUTPUT_PATH, "optimal_additional_capacities.png")
-            )
-            is True
-        )
 
     def teardown_class(self):
         """ """
