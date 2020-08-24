0ab430873417482e90a2bd678e94b13b6784c8d6
smartie2076 <martha.hoffmann@rl-institut.de>
2020-08-24 17:36:22 +0200

    Finalizing benchmark test for economic asset KPI

--- tests/test_benchmark_KPI.py
+++ tests/test_benchmark_KPI.py
@@ -17,7 +17,7 @@ import random
 from pytest import approx
 from mvs_eland_tool import main
 from src.B0_data_input_json import load_json
-from src.C0_data_processing import bus_suffix
+import src.C2_economic_functions as C2
 
 from .constants import (
     TEST_REPO_PATH,
@@ -31,30 +31,61 @@ from src.constants_json_strings import (
     ENERGY_PROVIDERS,
     ENERGY_STORAGE,
     ENERGY_CONSUMPTION,
+    INPUT_POWER,
+    OUTPUT_POWER,
+    STORAGE_CAPACITY,
     VALUE,
     FLOW,
-     LIFETIME_SPECIFIC_COST_OM,
-     LIFETIME_PRICE_DISPATCH,
-     LIFETIME_SPECIFIC_COST,
-     ANNUITY_SPECIFIC_INVESTMENT_AND_OM,
-     SIMULATION_ANNUITY,
-     SPECIFIC_REPLACEMENT_COSTS_INSTALLED,
-     SPECIFIC_REPLACEMENT_COSTS_OPTIMIZED,
-     OPTIMIZED_ADD_CAP,
-     ANNUITY_OM,
-     ANNUITY_TOTAL,
-     COST_TOTAL,
-     COST_OPERATIONAL_TOTAL,
-     COST_OM,
-     COST_DISPATCH,
-     COST_INVESTMENT,
-     COST_UPFRONT,
-     COST_REPLACEMENT,
-     LCOE_ASSET,
+    LIFETIME_SPECIFIC_COST_OM,
+    LIFETIME_PRICE_DISPATCH,
+    LIFETIME_SPECIFIC_COST,
+    ANNUITY_SPECIFIC_INVESTMENT_AND_OM,
+    SIMULATION_ANNUITY,
+    SPECIFIC_REPLACEMENT_COSTS_INSTALLED,
+    SPECIFIC_REPLACEMENT_COSTS_OPTIMIZED,
+    OPTIMIZED_ADD_CAP,
+    ANNUITY_OM,
+    ANNUITY_TOTAL,
+    COST_TOTAL,
+    COST_OPERATIONAL_TOTAL,
+    COST_OM,
+    COST_DISPATCH,
+    COST_INVESTMENT,
+    COST_UPFRONT,
+    COST_REPLACEMENT,
+    LCOE_ASSET,
+    CURR,
+    DISCOUNTFACTOR,
+    PROJECT_DURATION,
+    ANNUITY_FACTOR,
+    CRF,
 )
 
 TEST_INPUT_PATH = os.path.join(TEST_REPO_PATH, "benchmark_test_inputs")
 TEST_OUTPUT_PATH = os.path.join(TEST_REPO_PATH, "benchmark_test_outputs")
+
+dict_economic = {
+    CURR: "Euro",
+    DISCOUNTFACTOR: {VALUE: 0.08},
+    PROJECT_DURATION: {VALUE: 20},
+}
+
+dict_economic.update(
+    {
+        ANNUITY_FACTOR: {
+            VALUE: C2.annuity_factor(
+                project_life=dict_economic[PROJECT_DURATION][VALUE],
+                discount_factor=dict_economic[DISCOUNTFACTOR][VALUE],
+            )
+        },
+        CRF: {
+            VALUE: C2.crf(
+                project_life=dict_economic[PROJECT_DURATION][VALUE],
+                discount_factor=dict_economic[DISCOUNTFACTOR][VALUE],
+            )
+        },
+    }
+)
 
 
 class Test_Economic_KPI:
@@ -97,7 +128,6 @@ class Test_Economic_KPI:
         """
         use_case = "Economic_KPI_C2_E2"
 
-        """
         # Execute the script
         main(
             overwrite=True,
@@ -109,12 +139,95 @@ class Test_Economic_KPI:
 
         # read json with results file
         data = load_json(os.path.join(TEST_OUTPUT_PATH, use_case, JSON_WITH_RESULTS))
-        """
+
+        # Read expected values from file. To edit the values, please use the .xls file first and convert the first tab to csv.
         expected_value_file = "test_data_economic_expected_values.csv"
-        expected_values = pd.read_csv(os.path.join(TEST_INPUT_PATH, use_case, expected_value_file), sep=",")
-        print(expected_values)
-        # make sure grid is not used, ie. that diesel generator supplies all demand
-        assets = expected_values.columns()
-        groups = expected_values["group"]
-        print(assets)
-        print(groups)
+        expected_values = pd.read_csv(
+            os.path.join(TEST_INPUT_PATH, use_case, expected_value_file),
+            sep=",",
+            index_col=0,
+        )
+        # Define numbers in the csv as int/floats instead of str, but leave row "group" as a string
+        groups = expected_values.loc["group"]
+        expected_values = expected_values.convert_objects(convert_numeric=True)
+        expected_values.loc["group"] = groups
+        expected_values.loc[FLOW] = [0, 0, 0, 0, 0]
+
+        KEYS_TO_BE_EVALUATED = [
+            LIFETIME_SPECIFIC_COST_OM,
+            LIFETIME_PRICE_DISPATCH,
+            LIFETIME_SPECIFIC_COST,
+            ANNUITY_SPECIFIC_INVESTMENT_AND_OM,
+            SIMULATION_ANNUITY,
+            SPECIFIC_REPLACEMENT_COSTS_INSTALLED,
+            SPECIFIC_REPLACEMENT_COSTS_OPTIMIZED,
+            OPTIMIZED_ADD_CAP,
+            COST_INVESTMENT,
+            COST_UPFRONT,
+            COST_REPLACEMENT,
+            COST_OM,
+            COST_DISPATCH,
+            COST_OPERATIONAL_TOTAL,
+            COST_TOTAL,
+            ANNUITY_OM,
+            ANNUITY_TOTAL,
+            LCOE_ASSET,
+        ]
+
+        # Derive expected values dependent on actual dispatch of the asset(s)
+        for asset in expected_values.columns:
+            # determine asset dictionary (special for storages)
+            if asset in [INPUT_POWER, OUTPUT_POWER, STORAGE_CAPACITY]:
+                asset_data = data[expected_values[asset]["group"]]["storage_01"][asset]
+            else:
+                asset_data = data[expected_values[asset]["group"]][asset]
+            # Get dispatch of the assets
+            expected_values[asset][FLOW] = asset_data[FLOW]
+            # Calculate cost parameters that are dependent on the flow
+            expected_values[asset][COST_DISPATCH] = expected_values[asset][
+                LIFETIME_PRICE_DISPATCH
+            ] * sum(expected_values[asset][FLOW])
+            expected_values[asset][COST_OPERATIONAL_TOTAL] = (
+                expected_values[asset][COST_DISPATCH] + expected_values[asset][COST_OM]
+            )
+            expected_values[asset][COST_TOTAL] = (
+                expected_values[asset][COST_OPERATIONAL_TOTAL]
+                + expected_values[asset][COST_INVESTMENT]
+            )
+            # Process cost
+            expected_values[asset][ANNUITY_OM] = (
+                expected_values[asset][COST_OPERATIONAL_TOTAL]
+                * dict_economic[CRF][VALUE]
+            )
+            expected_values[asset][ANNUITY_TOTAL] = (
+                expected_values[asset][COST_TOTAL] * dict_economic[CRF][VALUE]
+            )
+            if sum(expected_values[asset][FLOW]) == 0:
+                expected_values[asset][LCOE_ASSET] = 0
+            else:
+                expected_values[asset][LCOE_ASSET] = expected_values[asset][
+                    ANNUITY_TOTAL
+                ] / sum(expected_values[asset][FLOW])
+
+        # Store to csv to enable manual check, eg. of LCOE_A. Only previously empty rows have been changed.
+        expected_values.drop("flow").to_csv(
+            os.path.join(TEST_INPUT_PATH, use_case, expected_value_file), sep=","
+        )
+
+        # Check if asset costs were correctly calculated in C2 and E2
+        for asset in expected_values.columns:
+            # determine asset dictionary (special for storages)
+            if asset in [INPUT_POWER, OUTPUT_POWER, STORAGE_CAPACITY]:
+                asset_data = data[expected_values[asset]["group"]]["storage_01"][asset]
+            else:
+                asset_data = data[expected_values[asset]["group"]][asset]
+            # assertion
+            for key in KEYS_TO_BE_EVALUATED:
+                print(asset)
+                assert expected_values[asset][key] == pytest.approx(
+                    asset_data[key][VALUE], rel=1e-3
+                ), f"Parameter {key} of asset {asset} is not of expected value."
+
+    def teardown_method(self):
+        if os.path.exists(TEST_OUTPUT_PATH):
+            shutil.rmtree(TEST_OUTPUT_PATH, ignore_errors=True)
