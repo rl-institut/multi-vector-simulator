c5bae75bb7c94449b0f0ad089154859ff0040471
smartie2076 <gitMA18rli>
2020-08-28 16:22:43 +0200

    Black

--- src/constants.py
+++ src/constants.py
@@ -256,14 +256,8 @@ EXTRA_CSV_PARAMETERS = {
 
 ENERGY_CARRIER_UNIT = "energy_carrier_unit"
 DEFAULT_WEIGHTS_ENERGY_CARRIERS = {
-    "Electricity": {
-        UNIT: "kWh_eleq/kWh_el",
-        VALUE: 1,
-        ENERGY_CARRIER_UNIT: "kWh_el"},
-    "Heat": {
-        UNIT: "KWh_eleq/kWh_therm",
-        VALUE: 1,
-        ENERGY_CARRIER_UNIT: "kWh_therm"},
+    "Electricity": {UNIT: "kWh_eleq/kWh_el", VALUE: 1, ENERGY_CARRIER_UNIT: "kWh_el"},
+    "Heat": {UNIT: "KWh_eleq/kWh_therm", VALUE: 1, ENERGY_CARRIER_UNIT: "kWh_therm"},
     "H2": {
         UNIT: "kWh_eleq/kgH2",
         VALUE: 32.87,
@@ -272,12 +266,12 @@ DEFAULT_WEIGHTS_ENERGY_CARRIERS = {
     "Diesel": {
         UNIT: "kWh_eleq/l",
         VALUE: 8.20,
-        ENERGY_CARRIER_UNIT: "l"
+        ENERGY_CARRIER_UNIT: "l",
     },  # https://epact.energy.gov/fuel-conversion-factors, convertion gallon->4.546092 l
     "Gas": {
         UNIT: "kWh_eleq/m3",
         VALUE: 5.38,
-        ENERGY_CARRIER_UNIT: "l"
+        ENERGY_CARRIER_UNIT: "l",
     },  # https://epact.energy.gov/fuel-conversion-factors, convertion gallon->4.546092 l
 }
 
