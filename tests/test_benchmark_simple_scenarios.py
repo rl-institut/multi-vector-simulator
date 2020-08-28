c841ee78ac4f91834bdb66c79c4eff3b8e57fe32
ursulaelmir <60609888+ursulaelmir@users.noreply.github.com>
2020-08-28 16:58:23 +0200

    Fix benchmark test for heat sector + electricity time series

--- tests/test_benchmark_simple_scenarios.py
+++ tests/test_benchmark_simple_scenarios.py
@@ -259,7 +259,7 @@ class TestACElectricityBus:
                     assert abs(battery_charge[i - 1]) > 0
 
     @pytest.mark.skipif(
-        EXECUTE_TESTS_ON not in (TESTS_ON_MASTER),
+        EXECUTE_TESTS_ON not in (TESTS_ON_MASTER, TESTS_ON_DEV, "testing_AFG"),
         reason="Benchmark test deactivated, set env variable "
         "EXECUTE_TESTS_ON to 'master' to run this test",
     )
@@ -283,19 +283,24 @@ class TestACElectricityBus:
             os.path.join(TEST_OUTPUT_PATH, use_case, "timeseries_all_busses.xlsx"),
             sheet_name=bus_suffix("Heat"),
         )
+        # create dict with electricity prices
+        electricity_price = data[ENERGY_PROVIDERS]["Electricity DSO"][ENERGY_PRICE][
+            VALUE
+        ]["data"]
         # compare cost of using heat pump with electricity price to heat price
-        if (
-            data[ENERGY_PROVIDERS]["Electricity DSO"][ENERGY_PRICE][VALUE]
-            / data[ENERGY_CONVERSION]["heat_pump"][EFFICIENCY][VALUE]
-            > data[ENERGY_PROVIDERS]["Heat DSO"][ENERGY_PRICE][VALUE]
-        ):
-            assert sum(busses_flow["Gas_consumption_period"]) == approx(
-                abs(sum(busses_flow["demand_heat"]))
-            )
-        else:
-            assert sum(busses_flow["heat_pump"]) == approx(
-                abs(sum(busses_flow["demand_heat"]))
-            )
+        for i in range(0, len(electricity_price)):
+            if (
+                electricity_price[i]
+                / data[ENERGY_CONVERSION]["heat_pump"][EFFICIENCY][VALUE]
+                > data[ENERGY_PROVIDERS]["Heat DSO"][ENERGY_PRICE][VALUE]
+            ):
+                assert busses_flow["Gas_consumption_period"][i] == approx(
+                    abs(busses_flow["demand_heat"][i])
+                )
+            else:
+                assert busses_flow["heat_pump"][i] == approx(
+                    abs(busses_flow["demand_heat"][i])
+                )
 
     def teardown_method(self):
         if os.path.exists(TEST_OUTPUT_PATH):
