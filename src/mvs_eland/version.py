62fcd3ff3b50fd30933df725fd6474e4aa1145d0
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-09-01 23:01:42 +0200

    Bump version number

--- src/mvs_eland/version.py
+++ src/mvs_eland/version.py
@@ -1,4 +1,4 @@
 # versioning scheme: Major release.Minor release.Patches
-version_num = "0.3.0"
+version_num = "0.4.0"
 # date format iso8601: YYYY-MM-DD
-version_date = "2020-06-08"
+version_date = "2020-09-01"
