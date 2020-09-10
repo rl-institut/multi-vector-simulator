af94d6e0c3f6d8b0fa731e49e51bd4628d134c34
SabineH <sabine.haas@rl-institut.de>
2020-09-10 15:43:41 +0200

    Write tests for time series checks of previous commit

--- tests/test_C1_verification.py
+++ tests/test_C1_verification.py
@@ -1,4 +1,7 @@
 import pytest
+import pandas as pd
+import os
+import json
 
 import mvs_eland.C1_verification as C1
 from _constants import (
@@ -9,6 +12,11 @@ from _constants import (
     ENERGY_PRICE,
     UNIT,
     VALUE,
+)
+from mvs_eland.utils.constants_json_strings import (
+    TIMESERIES_NORMALIZED,
+    RENEWABLE_ASSET_BOOL,
+    ENERGY_PRODUCTION,
 )
 
 
@@ -50,6 +58,52 @@ def test_check_feedin_tariff_not_greater_energy_price():
         }
     }
     C1.check_feedin_tariff(dict_values)
+
+
+def test_check_time_series_values_between_0_and_1_True():
+    time_series = pd.Series([0, 0.22, 0.5, 0.99, 1])
+    result = C1.check_time_series_values_between_0_and_1(time_series=time_series)
+    assert result == True
+
+
+def test_check_time_series_values_between_0_and_1_False_greater_1():
+    time_series = pd.Series([0, 0.22, 0.5, 0.99, 1, 1.01])
+    result = C1.check_time_series_values_between_0_and_1(time_series=time_series)
+    assert result == False
+
+
+def test_check_time_series_values_between_0_and_1_False_smaller_0():
+    time_series = pd.Series([0, 0.22, 0.5, 0.99, 1, -0.01])
+    result = C1.check_time_series_values_between_0_and_1(time_series=time_series)
+    assert result == False
+
+
+@pytest.fixture()
+def get_json():
+    """ Reads input json file and adds time series to non-dispatchable sources. """
+    with open(os.path.join(JSON_PATH)) as json_file:
+        dict_values = json.load(json_file)
+
+    def _add_time_series_to_dict_values(ts):
+        for key, source in dict_values[ENERGY_PRODUCTION].items():
+            if source[RENEWABLE_ASSET_BOOL][VALUE] == True:
+                dict_values[ENERGY_PRODUCTION][key][TIMESERIES_NORMALIZED] = ts
+        return dict_values
+
+    return _add_time_series_to_dict_values
+
+
+def test_check_non_dispatchable_source_time_series_passes(get_json):
+    # with open(os.path.join(JSON_PATH)) as json_file:
+    #     dict_values = json.load(json_file)
+    dict_values = get_json(pd.Series([0, 0.22, 0.5, 0.99, 1]))
+    C1.check_non_dispatchable_source_time_series(dict_values=dict_values)
+
+
+def test_check_non_dispatchable_source_time_series_results_in_error_msg(get_json):
+    dict_values = get_json(pd.Series([0, 0.22, 0.5, 0.99, 1, 1.01]))
+    return_value = C1.check_non_dispatchable_source_time_series(dict_values=dict_values)
+    assert return_value == False
 
 
 # def test_check_input_values():
