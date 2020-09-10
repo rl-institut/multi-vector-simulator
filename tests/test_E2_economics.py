f9f88f3999581f0497b84b1be28b98964ff4b3a8
smartie2076 <martha.hoffmann@rl-institut.de>
2020-09-10 12:03:32 +0200

    Formatting

--- tests/test_E2_economics.py
+++ tests/test_E2_economics.py
@@ -222,9 +222,10 @@ def test_calculate_annual_dispatch_expenditures_list_pd_series():
 
 def test_calculate_annual_dispatch_expenditures_nested_list():
     dispatch_expenditure = E2.calculate_dispatch_expenditures(
-            dispatch_price=[1, [1, 2]], flow=flow, asset=asset
-        )
-    assert dispatch_expenditure == 3+3+6
+        dispatch_price=[1, [1, 2]], flow=flow, asset=asset
+    )
+    assert dispatch_expenditure == 3 + 3 + 6
+
 
 def test_calculate_annual_dispatch_expenditures_str_error():
     with pytest.raises(TypeError):
