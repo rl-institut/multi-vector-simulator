ceec8b3204e7770761d2327f9e814946015dd8d3
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-09-23 16:51:53 +0200

    Explicitly list package

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(include=["mvs_eland"]),  # Required
+    packages=["mvs_eland"],  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. If you
