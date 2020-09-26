2dfa39c56b22ddff40b1f36b38d3605a205b6a1d
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-09-26 20:00:45 +0200

    Remove a useless sentence

--- tests/test_input_folder_parameters.py
+++ tests/test_input_folder_parameters.py
@@ -71,7 +71,6 @@ def test_input_folder_json_file_have_required_parameters(input_folder):
     if os.path.exists(os.path.join(input_folder, JSON_FNAME)):
         comparison = compare_input_parameters_with_reference(input_folder, ext=JSON_EXT)
         assert MISSING_PARAMETERS_KEY not in comparison, (
-            f"In path {input_folder}, the following parameters are missing:"
-            f" {MISSING_PARAMETERS_KEY} is included: "
+            f"In path {input_folder}/{JSON_FNAME}, the following parameters are missing:"
             + str(comparison[MISSING_PARAMETERS_KEY])
         )
