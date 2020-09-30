9653a844767720670abbebeaf613d9205fd3f983
SabineH <sabine.haas@rl-institut.de>
2020-09-30 18:44:30 +0200

    missing renaming of E4

--- src/mvs_eland/E0_evaluation.py
+++ src/mvs_eland/E0_evaluation.py
@@ -7,7 +7,7 @@ import mvs_eland.E1_process_results as E1
 import mvs_eland.E2_economics as E2
 import mvs_eland.E3_indicator_calculation as E3
 
-import mvs_eland.E4_verification_of_constraints as E4
+import mvs_eland.E4_verification as E4
 
 from mvs_eland.utils.constants_json_strings import (
     UNIT,
