5ce52ddc4e0774ba75a1100b24303f842e5884cd
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-11-13 00:15:02 +0100

    Black linting

--- src/multi_vector_simulator/E3_indicator_calculation.py
+++ src/multi_vector_simulator/E3_indicator_calculation.py
@@ -778,8 +778,9 @@ def equation_onsite_energy_fraction(total_generation, total_feedin):
     else:
         # TODO find a better way to deal with this
         onsite_energy_fraction = 0
-        logging.warning("The total local energy generation is zero, therefore the onsite energy fraction cannot be calculated and is set to 0")
-
+        logging.warning(
+            "The total local energy generation is zero, therefore the onsite energy fraction cannot be calculated and is set to 0"
+        )
 
     return onsite_energy_fraction
 
