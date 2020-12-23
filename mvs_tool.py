87655e372d00cb47bd121f77c15117a094b381e7
paragpatil39 <paragpatil39@gmail.com>
2020-12-23 23:17:06 +0100

    Update: pytests for timeseries fucntions

--- mvs_tool.py
+++ mvs_tool.py
@@ -1,10 +1,4 @@
 from multi_vector_simulator.cli import main
 
 if __name__ == "__main__":
-    main(
-        overwrite=True,
-        input_type="csv",
-        path_input_folder="tests/benchmark_test_inputs/Feature_parameters_as_timeseries",
-        save_png=True,
-        pdf_report=True,
-    )
+    main()
