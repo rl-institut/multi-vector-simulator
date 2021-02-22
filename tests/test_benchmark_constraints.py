c1a269705f669df01f8feb0dd7dc1c9fc2360549
SabineH <sabine.haas@rl-institut.de>
2021-02-22 15:07:51 +0100

    Adapt precision in benchmark test

--- tests/test_benchmark_constraints.py
+++ tests/test_benchmark_constraints.py
@@ -330,15 +330,16 @@ class Test_Constraints:
             degree_of_nze[use_case[0]] < 1
         ), f"The degree of NZE of a LES without NZE constraint should be lower than 1 but is {degree_of_nze[use_case[0]]}."
         assert (
-            degree_of_nze[use_case[1]] >= 1
+            degree_of_nze[use_case[1]] >= 1 - 10 ** (-6)
         ), f"The degree of NZE of a LES with NZE constraint should be equal or greater than 1 but is {degree_of_nze[use_case[1]]}."
+
         assert (
             degree_of_nze[use_case[2]] < 1
         ), f"The degree of NZE of a sector-coupled LES without NZE constraint should be lower than 1 but is {degree_of_nze[use_case[2]]}."
 
         balance = feedin_to_grid[use_case[3]] - consumption_from_grid[use_case[3]]
         assert (
-            balance.round(2) >= 0
+            balance >= 0 - 10 ** (-2)
         ), f"The balance between feed-in and consumption from grid of the sector-coupled LES with NZE constraint (feed-in - consumption) should be equal or greater than 0 but is {balance}."
 
     def teardown_method(self):
