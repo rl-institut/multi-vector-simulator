85afeb0b41700feffb1f36df0f7977f6334db1fa
SabineH <sabine.haas@rl-institut.de>
2021-03-17 09:12:23 +0100

    Add check for correct installedcap processing to AB_grid_pv benchmark test

--- tests/test_benchmark_scenarios.py
+++ tests/test_benchmark_scenarios.py
@@ -12,8 +12,9 @@ import json
 import mock
 import pandas as pd
 import pytest
-
 from pytest import approx
+from pandas.util.testing import assert_series_equal
+
 from multi_vector_simulator.cli import main
 from multi_vector_simulator.server import run_simulation
 from multi_vector_simulator.B0_data_input_json import load_json
@@ -32,6 +33,8 @@ from _constants import (
 from multi_vector_simulator.utils.constants import (
     JSON_WITH_RESULTS,
     JSON_FILE_EXTENSION,
+    CSV_ELEMENTS,
+    TIME_SERIES
 )
 
 from multi_vector_simulator.utils.constants_json_strings import (
@@ -44,6 +47,10 @@ from multi_vector_simulator.utils.constants_json_strings import (
     ENERGY_CONSUMPTION,
     FLOW,
     EFFICIENCY,
+    ENERGY_PRODUCTION,
+    INSTALLED_CAP,
+    SIMULATION_SETTINGS,
+    EVALUATED_PERIOD,
 )
 
 from multi_vector_simulator.utils.data_parser import convert_epa_params_to_mvs
@@ -71,6 +78,7 @@ class TestACElectricityBus:
     def test_benchmark_AB_grid_pv(self, margs):
         r"""
         Benchmark test for simple case grid connected PV. Since the PV is already installed, this tests makes sure that the PV generation is totally used to supply the load and the rest in take from the grid.
+        Additionally, in this test it is made sure that the results PV time series equals the input time series multiplied by installed capacity. This ensures that `installedCap` is processed correctly within the model.
         """
         use_case = "AB_grid_PV"
         main(
@@ -92,6 +100,39 @@ class TestACElectricityBus:
 
         # make sure the sum of the bus flow is always zero (there are rounding errors)
         assert df_busses_flow.net_sum.map(lambda x: 0 if x < 1e-4 else 1).sum() == 0
+
+        # make sure that installedCap is processed correctly - pv time series of results
+        # equal input pv time series times installedCap
+        # get pv input time series and evaluated period (to shorten time series)
+        input_time_series_pv = pd.read_csv(
+            os.path.join(TEST_INPUT_PATH, use_case, TIME_SERIES, "pv_solar_input.csv")
+        )
+        simulation_settings = pd.read_csv(
+            os.path.join(
+                TEST_INPUT_PATH, use_case, CSV_ELEMENTS, f"{SIMULATION_SETTINGS}.csv"
+            )
+        ).set_index("Unnamed: 0")
+        evaluated_period = float(
+            simulation_settings[SIMULATION_SETTINGS][EVALUATED_PERIOD]
+        )
+        # shorten input pv time series according to `evaluated_period`
+        input_time_series_pv_shortened = input_time_series_pv[
+            : int(evaluated_period * 24)
+        ]["kW"]
+        # get result time series and installed pv capacity
+        result_time_series_pv = df_busses_flow["pv_plant_01"]
+        energy_production_data = pd.read_csv(
+            os.path.join(
+                TEST_INPUT_PATH, use_case, CSV_ELEMENTS, f"{ENERGY_PRODUCTION}.csv"
+            )
+        ).set_index("Unnamed: 0")
+        installed_capacity = float(energy_production_data["pv_plant_01"][INSTALLED_CAP])
+        # adapt index
+        result_time_series_pv.index = input_time_series_pv_shortened.index
+
+        assert_series_equal(
+            result_time_series_pv,input_time_series_pv_shortened * installed_capacity, check_names=False
+        )
 
     # this ensure that the test is only ran if explicitly executed, ie not when the `pytest` command
     # alone is called
