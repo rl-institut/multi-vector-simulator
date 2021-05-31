a43e19c92b478b5f91b5bae195ab9189310d8565
smartie2076 <martha.hoffmann@rl-institut.de>
2021-05-31 15:53:13 +0200

    Bump version number

--- src/multi_vector_simulator/version.py
+++ src/multi_vector_simulator/version.py
@@ -1,4 +1,4 @@
 # versioning scheme: Major release.Minor release.Patches
-version_num = "0.5.6dev"
+version_num = "1.0.0"
 # date format iso8601: YYYY-MM-DD
-version_date = "2021-03-04"
+version_date = "2021-05-31"
