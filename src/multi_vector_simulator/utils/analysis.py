491c69ca593be196780384177d22680eea1cfa0d
smartie2076 <martha.hoffmann@rl-institut.de>
2021-07-06 10:19:47 +0200

    Remove excess code for pvcompare

--- src/multi_vector_simulator/utils/analysis.py
+++ src/multi_vector_simulator/utils/analysis.py
@@ -109,7 +109,7 @@ def single_param_variation_analysis(
     print({"parameters": param_values, "outputs": answer})
     return {"parameters": param_values, "outputs": answer}
 
-import pvcompare.main as main
+from multi_vector_simulator.cli import main
 import pvcompare.constants as constants
 import os
 import pandas as pd
@@ -169,325 +169,8 @@ def create_loop_output_structure(outputs_directory, scenario_name, variable_name
 
     return loop_output_directory
 
-
-def loop_pvcompare(
-    scenario_name,
-    latitude,
-    longitude,
-    years,
-    storeys,
-    country,
-    loop_type,
-    loop_dict=None,
-    pv_setup=None,
-    user_inputs_mvs_directory=None,
-    outputs_directory=None,
-    user_inputs_pvcompare_directory=None,
-):
-    """
-    Starts multiple *pvcompare* simulations with a range of values for a
-    specific loop type.
-
-    The loop type corresponds to a variable or a set of
-    variables that is/are changed in each loop.The
-    results, stored in two excel sheets, are copied into `loop_output_directory`.
-
-    Parameters
-    ----------
-    scenario_name: str
-        Name of the Scenario. The name should follow the scheme:
-        "Scenario_A1", "Scenario_A2", "Scenario_B1" etc.
-    latitude: float
-        latitude of the location
-    longitude: foat
-        longitude of the location
-    years: list
-        year(s) of simulation
-    storeys: int
-        number of storeys
-    country: str
-        country of location
-    loop_type: str
-        possible values: 'location', 'year', 'storeys', 'technology', 'hp_temp'.
-        Defines the variable or variables that are changed with each loop.
-    loop_dict: dict
-        For location, the form of the dict should be: {"step1": ["country", "lat", "lon"], "step2": ["country", "lat", "lon"], etc}.
-        For technology, the form of the dict should be: {"step1": "si", "step2": "cpv", "step3": "psi"}
-        For year/storeys/hp_temp, the form of the dict should be: {"start": "1", "stop": "10", "step": "2"}
-    pv_setup: dict or None
-        If `pv_setup` is None, it is loaded from the `input_directory/pv_setup.cvs`.
-        Default: None.
-    user_inputs_mvs_directory: str or None
-        Default: `user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY`
-    outputs_directory: str or None
-        Path to output directory.
-        Default: `outputs_directory = constants.DEFAULT_OUTPUTS_DIRECTORY`
-    user_inputs_pvcompare_directory: str or None
-        If None, `constants.DEFAULT_USER_INPUTS_PVCOMPARE_DIRECTORY` is used
-        as user_input_directory. Default: None.
-
-    Returns
-    -------
-        None
-    """
-    # checks of outputs_directory and user_inputs_mvs_directory is None
-    if user_inputs_mvs_directory == None:
-        user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY
-    if outputs_directory == None:
-        outputs_directory = constants.DEFAULT_OUTPUTS_DIRECTORY
-    if user_inputs_pvcompare_directory == None:
-        user_inputs_pvcompare_directory = (
-            constants.DEFAULT_USER_INPUTS_PVCOMPARE_DIRECTORY
-        )
-
-    loop_output_directory = create_loop_output_structure(
-        outputs_directory=outputs_directory,
-        scenario_name=scenario_name,
-        variable_name=loop_type,
-    )
-
-    for year in years:
-        if loop_type is "location":
-            for key in loop_dict:
-                country = loop_dict[key][0]
-                latitude = loop_dict[key][1]
-                longitude = loop_dict[key][2]
-
-                single_loop_pvcompare(
-                    scenario_name=scenario_name,
-                    storeys=storeys,
-                    country=country,
-                    latitude=latitude,
-                    longitude=longitude,
-                    year=year,
-                    user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-                    user_inputs_mvs_directory=user_inputs_mvs_directory,
-                    outputs_directory=outputs_directory,
-                    plot=False,
-                    pv_setup=pv_setup,
-                    loop_output_directory=loop_output_directory,
-                    step=str(latitude) + "_" + str(longitude),
-                    loop_type=loop_type,
-                )
-
-        elif loop_type is "year":
-
-            year = loop_dict["start"]
-            while year <= loop_dict["stop"]:
-
-                single_loop_pvcompare(
-                    scenario_name=scenario_name,
-                    storeys=storeys,
-                    country=country,
-                    latitude=latitude,
-                    longitude=longitude,
-                    year=year,
-                    user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-                    user_inputs_mvs_directory=user_inputs_mvs_directory,
-                    outputs_directory=outputs_directory,
-                    plot=False,
-                    pv_setup=pv_setup,
-                    loop_output_directory=loop_output_directory,
-                    step=year,
-                    loop_type=loop_type,
-                )
-                year = year + loop_dict["step"]
-
-        elif loop_type is "storeys":
-
-            number_of_storeys = loop_dict["start"]
-            while number_of_storeys <= loop_dict["stop"]:
-
-                single_loop_pvcompare(
-                    scenario_name=scenario_name,
-                    storeys=number_of_storeys,
-                    country=country,
-                    latitude=latitude,
-                    longitude=longitude,
-                    year=year,
-                    user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-                    user_inputs_mvs_directory=user_inputs_mvs_directory,
-                    outputs_directory=outputs_directory,
-                    plot=False,
-                    pv_setup=pv_setup,
-                    loop_output_directory=loop_output_directory,
-                    step=number_of_storeys,
-                    loop_type=loop_type,
-                )
-
-                number_of_storeys = number_of_storeys + loop_dict["step"]
-
-        elif loop_type is "technology":
-
-            for key in loop_dict:
-                technology = loop_dict[key]
-
-                data_path = os.path.join(
-                    user_inputs_pvcompare_directory, "pv_setup.csv"
-                )
-                # load input parameters from pv_setup.csv
-                pv_setup = pd.read_csv(data_path)
-                for i, row in pv_setup.iterrows():
-                    pv_setup.at[i, "technology"] = technology
-                pv_setup.to_csv(data_path, index=False)
-
-                single_loop_pvcompare(
-                    scenario_name=scenario_name,
-                    storeys=storeys,
-                    country=country,
-                    latitude=latitude,
-                    longitude=longitude,
-                    year=year,
-                    user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-                    user_inputs_mvs_directory=user_inputs_mvs_directory,
-                    outputs_directory=outputs_directory,
-                    plot=False,
-                    pv_setup=None,
-                    loop_output_directory=loop_output_directory,
-                    step=technology,
-                    loop_type=loop_type,
-                )
-
-        elif loop_type is "hp_temp":
-            temp_high = loop_dict["start"]
-
-            data_path = os.path.join(
-                user_inputs_pvcompare_directory, "heat_pumps_and_chillers.csv"
-            )
-            while temp_high <= loop_dict["stop"]:
-                # load input parameters from pv_setup.csv
-                hp_file = pd.read_csv(data_path, index_col=0)
-                hp_file.at["heat_pump", "temp_high"] = temp_high
-                hp_file.to_csv(data_path)
-
-                single_loop_pvcompare(
-                    scenario_name=scenario_name,
-                    storeys=storeys,
-                    country=country,
-                    latitude=latitude,
-                    longitude=longitude,
-                    year=year,
-                    user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-                    user_inputs_mvs_directory=user_inputs_mvs_directory,
-                    outputs_directory=outputs_directory,
-                    plot=False,
-                    pv_setup=pv_setup,
-                    loop_output_directory=loop_output_directory,
-                    step=temp_high,
-                    loop_type=loop_type,
-                )
-                temp_high = temp_high + loop_dict["step"]
-
-    logging.info("starting postprocessing KPI")
-    postprocessing_kpi(
-        scenario_name=scenario_name,
-        variable_name=loop_type,
-        outputs_directory=outputs_directory,
-    )
-
-
-def single_loop_pvcompare(
-    scenario_name,
-    storeys,
-    country,
-    latitude,
-    longitude,
-    year,
-    user_inputs_pvcompare_directory,
-    user_inputs_mvs_directory,
-    outputs_directory,
-    plot,
-    pv_setup,
-    loop_output_directory,
-    loop_type,
-    step,
-):
-    """
-
-    Parameters
-    ----------
-    scenario_name: str
-        name of the scenario.
-    storeys: int
-        number of storeys
-    country: str
-        country name
-    latitude: float
-        latitude of the location
-    longitude: foat
-        longitude of the location
-    year: int
-        year
-    user_inputs_pvcompare_directory: str or None
-        If None, `constants.DEFAULT_USER_INPUTS_PVCOMPARE_DIRECTORY` is used
-        as user_input_directory. Default: None.
-    user_inputs_mvs_directory: str or None
-        Default: `user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY`
-    outputs_directory: str or None
-        Path to output directory.
-        Default: `outputs_directory = constants.DEFAULT_OUTPUTS_DIRECTORY`
-    plot: bool
-        default: False
-    loop_output_directory: str
-        output directory defined in 'pvcompare.outputs.create_loop_output_structure()'.
-    loop_type: str
-        possible values: 'location', 'year', 'storeys', 'technology', 'hp_temp'.
-        Defines the variable or variables that are changed with each loop.
-    step: str or int
-        Gradation of the loop variable.
-
-    Returns
-    -------
-        None
-    """
-
-    main.apply_pvcompare(
-        storeys=storeys,
-        country=country,
-        latitude=latitude,
-        longitude=longitude,
-        year=year,
-        user_inputs_pvcompare_directory=user_inputs_pvcompare_directory,
-        user_inputs_mvs_directory=user_inputs_mvs_directory,
-        plot=plot,
-        pv_setup=pv_setup,
-    )
-
-    # define mvs_output_directory for every looping step
-    mvs_output_directory = os.path.join(
-        outputs_directory,
-        scenario_name,
-        "mvs_outputs_loop_" + str(loop_type) + "_" + str(year) + "_" + str(step),
-    )
-
-    main.apply_mvs(
-        scenario_name,
-        user_inputs_mvs_directory=user_inputs_mvs_directory,
-        mvs_output_directory=mvs_output_directory,
-        outputs_directory=outputs_directory,
-    )
-
-    excel_file1 = "scalars.xlsx"
-    new_excel_file1 = "scalars_" + str(year) + "_" + str(step) + ".xlsx"
-    src_dir = os.path.join(mvs_output_directory, excel_file1)
-    dst_dir = os.path.join(loop_output_directory, "scalars", new_excel_file1)
-    shutil.copy(src_dir, dst_dir)
-
-    excel_file2 = "timeseries_all_busses.xlsx"
-    new_excel_file2 = (
-        "timeseries_all_busses_" + "_" + str(year) + "_" + str(step) + ".xlsx"
-    )
-    src_dir = os.path.join(mvs_output_directory, excel_file2)
-    dst_dir = os.path.join(loop_output_directory, "timeseries", new_excel_file2)
-    shutil.copy(src_dir, dst_dir)
-
-
 def loop_mvs(
-    latitude,
-    longitude,
     years,
-    storeys,
-    country,
     variable_name,
     variable_column,
     csv_file_variable,
@@ -508,33 +191,35 @@ def loop_mvs(
 
     Parameters
     ----------
-    latitude: float
-        latitude of the location
-    longitude: foat
-        longitude of the location
+
     years: list
         year(s) for simulation
-    storeys:int
-        number of storeys
-    country: str
-        country of location
+
     variable_name: str
         name of the variable that is atapted in each loop
+
     variable_column: str
         name of the  variable column in the csv file
+
     csv_file_variable: str
         name of the csv file the variable is saved in
+
     start: int
         first value of the variable
+
     stop: int
         last value of the variable. notice that stop > start
+
     step: int
         step of increase
+
     scenario_name: str
         Name of the Scenario. The name should follow the scheme:
         "Scenario_A1", "Scenario_A2", "Scenario_B1" etc.
+
     user_inputs_mvs_directory: str or None
         Default: `user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY`
+
     outputs_directory: str or None
         Path to output directory.
         Default: `outputs_directory = constants.DEFAULT_OUTPUTS_DIRECTORY`
@@ -558,16 +243,6 @@ def loop_mvs(
 
     # loop over years
     for year in years:
-        # apply pvcompare
-        main.apply_pvcompare(
-            latitude=latitude,
-            longitude=longitude,
-            year=year,
-            storeys=storeys,
-            country=country,
-            user_inputs_mvs_directory=user_inputs_mvs_directory,
-        )
-
         # loop over the variable
         i = start
         while i <= stop:
@@ -588,8 +263,9 @@ def loop_mvs(
                 + str(i),
             )
 
+
             # apply mvs for every looping step
-            main.apply_mvs(
+            apply_mvs(
                 scenario_name=scenario_name,
                 mvs_output_directory=mvs_output_directory,
                 user_inputs_mvs_directory=user_inputs_mvs_directory,
@@ -633,6 +309,153 @@ def loop_mvs(
         outputs_directory=outputs_directory,
     )
 
+
+def apply_mvs(
+    scenario_name,
+    user_inputs_mvs_directory=None,
+    outputs_directory=None,
+    mvs_output_directory=None,
+):
+    r"""
+    Starts the energy system optimization with MVS and stores results.
+    Parameters
+    ----------
+    scenario_name: str
+        Name of the Scenario.
+    user_inputs_mvs_directory: str or None
+        Path to input directory containing files that describe the energy
+        system and that are an input to MVS. If None,
+        `constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY` is used.
+        Default: None.
+    outputs_directory: str
+        Path to output directory where results are saved in case `mvs_output_directory`
+        is None. If None, `constants.DEFAULT_OUTPUTS_DIRECTORY` is used.
+        Default: None.
+    mvs_output_directory: str or None
+        Path to output directory where results are saved. If None, it is filled in
+        automatically according to `outputs_directory` and `scenario_name`:
+        'outputs_directory/scenario_name/mvs_output'.
+        Default: None.
+    Returns
+    -------
+        Stores simulation results in directory according to `outputs_directory` and `scenario_name` in 'outputs_directory/scenario_name/mvs_outputs'.
+    """
+
+    if user_inputs_mvs_directory is None:
+        user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY
+    if outputs_directory is None:
+        outputs_directory = constants.DEFAULT_OUTPUTS_DIRECTORY
+    if not os.path.isdir(outputs_directory):
+        os.mkdir(outputs_directory)
+
+    scenario_folder = os.path.join(outputs_directory, scenario_name)
+    if mvs_output_directory is None:
+        mvs_output_directory = os.path.join(scenario_folder, "mvs_outputs")
+    # check if output folder exists, if not: create it
+    if not os.path.isdir(scenario_folder):
+        # create output folder
+        os.mkdir(scenario_folder)
+    # check if mvs_output_directory already exists. If yes, raise error
+    if os.path.isdir(mvs_output_directory):
+        raise NameError(
+            f"The mvs output directory {mvs_output_directory} "
+            f"already exists. Please delete the folder or "
+            f"rename 'scenario_name' to create a different scenario "
+            f"folder."
+        )
+
+    # adapt parameter 'scenario_name' in 'project_data.csv'.
+    add_scenario_name_to_project_data(
+        user_inputs_mvs_directory, scenario_name
+    )
+
+    main(
+        path_input_folder=user_inputs_mvs_directory,
+        path_output_folder=mvs_output_directory,
+        input_type="csv",
+        overwrite=True,
+        save_png=True,
+    )
+
+
+def add_scenario_name_to_project_data(user_inputs_mvs_directory, scenario_name):
+    r"""
+    Matches user input `scenario_name` with `scenario_name` in 'project_data.csv'.
+    If user input `scenario_name` is different to the parameter in
+    'project_data.csv', a warning is returned and 'project_data.csv' is
+    overwritten.
+    Parameters
+    ----------
+    user_inputs_mvs_directory: str or None
+        Path to MVS specific input directory. If None,
+        `constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY` is used.
+        Default: None.
+    scenario_name: str
+        Name of the Scenario.
+    Returns
+    -------
+    None
+    """
+    add_parameter_to_mvs_file(
+        user_inputs_mvs_directory=user_inputs_mvs_directory,
+        mvs_filename="project_data.csv",
+        mvs_row="scenario_name",
+        mvs_column="project_data",
+        pvcompare_parameter=scenario_name,
+        warning=True,
+    )
+def add_parameter_to_mvs_file(
+    user_inputs_mvs_directory,
+    mvs_filename,
+    mvs_row,
+    mvs_column,
+    pvcompare_parameter,
+    warning=True,
+):
+    r"""
+    Overwrites a value from a file in 'mvs_inputs/csv_elements' with a user input.
+    Parameters
+    ----------
+    user_inputs_mvs_directory: str or None
+        Path to MVS specific input directory. If None,
+        `constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY` is used.
+        Default: None.
+    mvs_filename: str
+        Name of the mvs-csv file.
+    mvs_row: str
+        Row name of the value in `mvs_filename`.
+    mvs_column: str
+        Column name of the value in `mvs_filename`.
+    pvcompare_parameter: str
+        Parameter that should be added to the mvs_csv file.
+    warning: bool
+        If True, a warning is returned that the parameter with the name
+        `mvs_row` is overwritten.
+    Returns
+    ------
+    None
+    """
+    if user_inputs_mvs_directory == None:
+        user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY
+
+    filename = os.path.join(user_inputs_mvs_directory, "csv_elements", mvs_filename)
+    # load mvs_csv_file
+    mvs_file = pd.read_csv(filename, index_col=0)
+
+    if warning is True:
+        if mvs_file.at[mvs_row, mvs_column] != pvcompare_parameter:
+            logging.warning(
+                f"The parameter {pvcompare_parameter} differs from "
+                f"the parameter {mvs_row} in {mvs_filename} and thus will "
+                f"be overwritten."
+            )
+
+    mvs_file.loc[[mvs_row], [mvs_column]] = pvcompare_parameter
+    mvs_file.to_csv(filename)
+    logging.info(
+        f"The parameter {mvs_row} has been added to the "
+        f"mvs input file {mvs_filename}."
+    )
 
 def postprocessing_kpi(
     scenario_name,
