e8bc00d5068984cf5b08a6dc4907e27c808942dd
smartie2076 <martha.hoffmann@rl-institut.de>
2022-03-11 13:02:40 +0100

    Update version number - Patch now easily identifiable in dev - Will also be shown in the top line of the autoreports (so that no non-fixed simulations are used for the reports)

--- src/multi_vector_simulator/version.py
+++ src/multi_vector_simulator/version.py
@@ -1,4 +1,4 @@
 # versioning scheme: Major release.Minor release.Patches
-version_num = "1.0.1dev"
+version_num = "1.0.2dev"
 # date format iso8601: YYYY-MM-DD
-version_date = "2021-05-31"
+version_date = "2022-03-11"
