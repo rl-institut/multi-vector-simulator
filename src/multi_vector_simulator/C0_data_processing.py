8f177686a5bb1c159ed8349f1bd6c6ff928b89df
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-04-07 12:37:13 +0200

    Fix typos in warning messages

--- src/multi_vector_simulator/C0_data_processing.py
+++ src/multi_vector_simulator/C0_data_processing.py
@@ -782,12 +782,12 @@ def change_sign_of_feedin_tariff(dict_feedin_tariff, dso):
             f"The {FEEDIN_TARIFF} of {dso} is positive, which means that feeding into the grid results in a revenue stream."
         )
     elif dict_feedin_tariff[VALUE] == 0:
-        # Add a warning msg in case the feedin induces expenses rather then revenue
-        logging.info(
+        # Add a warning msg in case the feedin induces expenses rather than revenue
+        logging.warning(
             f"The {FEEDIN_TARIFF} of {dso} is 0, which means that there is no renumeration for feed-in to the grid. Potentially, this can lead to random dispatch into feed-in and excess sinks."
         )
     elif dict_feedin_tariff[VALUE] < 0:
-        # Add a warning msg in case the feedin induces expenses rather then revenue
+        # Add a warning msg in case the feedin induces expenses rather than revenue
         logging.warning(
             f"The {FEEDIN_TARIFF} of {dso} is negative, which means that payments are necessary to be allowed to feed-into the grid. If you intended a revenue stream, set the feedin tariff to a positive value."
         )
