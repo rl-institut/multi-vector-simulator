09d8c66bc3f3dee8492afd1a7d44db0d0444050f
pebasantaf <pedrobasantafranco@outlook.com>
2022-07-06 13:48:24 +0200

    Identation modification

--- src/multi_vector_simulator/utils/__init__.py
+++ src/multi_vector_simulator/utils/__init__.py
@@ -518,7 +518,8 @@ def copy_report_assets(path_destination_folder):
                 os.path.join(PACKAGE_DATA_PATH, "assets"), assets_folder
             )
         except FileNotFoundError:
-            assets_folder = shutil.copytree(os.path.join(REPO_PATH, "multi-vector-simulator", "report", "assets"), assets_folder)
+            assets_folder = shutil.copytree(os.path.join(REPO_PATH, "multi-vector-simulator", 
+                                                         "report", "assets"), assets_folder)
     else:
         logging.warning(
             "The assets folder {} exists already, it will not be replaced by default folder "
