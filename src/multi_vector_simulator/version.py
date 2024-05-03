03bf1fa0a9819aaae275a1cad344d60682a42b2d
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2024-05-03 11:43:28 +0200

    Bump version number

--- src/multi_vector_simulator/version.py
+++ src/multi_vector_simulator/version.py
@@ -1,4 +1,4 @@
 # versioning scheme: Major release.Minor release.Patches
-version_num = "1.1.0rc2"
+version_num = "1.1.1"
 # date format iso8601: YYYY-MM-DD
-version_date = "2024-04-27"
+version_date = "2024-05-03"
